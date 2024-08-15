1ed2a1f1804908a868a23df79d9ae12f9b673c66
Jason (Siyu) Zhu <jasonchu13@outlook.com>
2024-08-15 10:01:25 -0700

    Add medusa patching code and example job with memory efficient Liger Kernel (#11)
    # Summary
    
    Add medusa patching code and example job with memory efficient Liger
    Kernel
    
    Medusa is a simple framework that democratizes the acceleration
    techniques for LLM generation with multiple decoding heads.
    [[repo](https://arxiv.org/abs/2401.10774)],
    [[paper](https://arxiv.org/abs/2401.10774)]
    
    During training, Medusa requires adding \(k\) decoding heads to the
    hidden states right before the regular LM head \(h_t\). The \(k\)-th
    head is used to predict the token in the \((t + k + 1)\)-th position of
    the next tokens (the original language model head is used to predict the
    \((t + 1)\)-th position).
    
    The Liger fused CE kernel is highly effective in this scenario,
    eliminating the need to materialize logits for each head, which usually
    consumes a large volume of memory due to the extensive vocabulary size
    (e.g., for LLaMA-3, the vocabulary size is 128k). The introduction of
    multiple heads can easily lead to OOM (Out of Memory) issues. However,
    thanks to the efficient Liger fused CE, which calculates the gradient in
    place and doesn't materialize the logits, we have observed very
    effective results. This efficiency opens up more opportunities for
    multi-token prediction research and development.
    
    
    # List of changes
    
    * Add self-contained medusa training flow with corresponding fsdp config
    * Add README.md with benchmark result
    * Add patching code to apply multi-head to backbone model
    
    
    ### TODO ([JasonZhu1313](https://github.com/JasonZhu1313))
    * [P0] Clean up the code and remove hard-code coefficient in patching
    code
    * [P1] parallelize the llm head inference code
    
    # Testing done
    Convergence test
    
    Liger kernel is very effective in memory usage for medusa training: 
    
    ![memory_Stage1_num_head_3](https://github.com/user-attachments/assets/d80f5e6d-87a2-4480-bbd5-eae83e1e15a1)
    
    ![memory_Stage1_num_head_5](https://github.com/user-attachments/assets/744bd04b-524e-42b9-b3e4-da1e9a48352c)
    
    ![memory_Stage2_num_head_3](https://github.com/user-attachments/assets/0ae916eb-6396-4c75-81e3-1b40da2c054c)
    
    ![memory_Stage2_num_head_5](https://github.com/user-attachments/assets/b862d614-8dd5-47d4-b8f4-ed90531480ae)
    
    ---------
    
    Co-authored-by: jaszhu <jzhu@linkedin.com>
    Co-authored-by: ByronHsu <byronhsu1230@gmail.com>

---
+++ examples/medusa/scripts/llama3_8b_medusa.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+export GPUS_PER_NODE=$(nvidia-smi --list-gpus | wc -l)
+export LOCAL_WORLD_SIZE=$GPUS_PER_NODE
+export NUM_NODES=$WORLD_SIZE
+export WORLD_SIZE=$((GPUS_PER_NODE * NUM_NODES))
+echo "Starting training... Num nodes: $NUM_NODES, Num workers: $WORLD_SIZE"
+
+export OUTPUT_DIR="/shared/user/Meta-Llama-3-70B-Instruct-code-act-3ep"
+export DATA_PATH="/shared/public/data/jaszhu/medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json"
+
+export LOCAL_TRAIN_BATCH_SIZE=4
+export GRADIENT_ACCUMULATION_STEPS=1
+export LR=1e-5
+
+export MEDUSA_NUM_HEADS=5
+export MEDUSA_NUM_LAYERS=1
+export MEDUSA_HEADS_COEFFICIENT=0.2
+export MEDUSA_DECAY_COEFFICIENT=0.8
+export MEDUSA_SCHEDULER=constant
+export MEDUSA_LR_MULTIPLIER=4.0
+
+accelerate launch --config_file fsdp/acc-fsdp.conf \
+    --num_machines $NUM_NODES \
+    --num_processes $WORLD_SIZE \
+    --main_process_ip $MASTER_ADDR \
+    --main_process_port $MASTER_PORT \
+    --machine_rank $RANK \
+    train.py \
+    --model_name_or_path /shared/public/models/Meta-Llama-3-8B-Instruct \
+    --data_path $DATA_PATH \
+    --bf16 True \
+    --output_dir $OUTPUT_DIR \
+    --num_train_epochs 10 \
+    --per_device_train_batch_size $LOCAL_TRAIN_BATCH_SIZE \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
+    --evaluation_strategy "no" \
+    --save_strategy "no" \
+    --prediction_loss_only \
+    --learning_rate $LR \
+    --weight_decay 0. \
+    --warmup_ratio 0.04 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --model_max_length 1024 \
+    --gradient_checkpointing True \
+    --lazy_preprocess False \
+    --report_to none \
+    --include_num_input_tokens_seen \
+    --medusa_num_heads $MEDUSA_NUM_HEADS \
+    --medusa_num_layers $MEDUSA_NUM_LAYERS \
+    --medusa_heads_coefficient $MEDUSA_HEADS_COEFFICIENT \
+    --medusa_decay_coefficient $MEDUSA_DECAY_COEFFICIENT \
+    --medusa_scheduler $MEDUSA_SCHEDULER \
+    --medusa_lr_multiplier $MEDUSA_LR_MULTIPLIER \
+    --medusa_only_heads False \
+    --medusa_return True \
+    --with_liger True
---
+++ examples/medusa/train.py
@@ -0,0 +1,371 @@
+# This code is based on tatsu-lab/stanford_alpaca. Below is the original copyright:
+#
+#    Copyright 2023 Rohan Taori, Ishaan Gulrajani, Tianyi Zhang, Yann Dubois, Xuechen Li
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Adapted from: https://github.com/lm-sys/FastChat/blob/main/fastchat/train/train.py
+
+import json
+import pathlib
+from dataclasses import dataclass, field
+from typing import Dict, Optional
+
+import torch
+import transformers
+from callback import EfficiencyCallback
+from medusa_util import add_medusa_heads
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+from transformers import Trainer
+from transformers.trainer_pt_utils import LabelSmoother
+
+from liger_kernel.transformers import apply_liger_kernel_to_llama
+
+IGNORE_TOKEN_ID = LabelSmoother.ignore_index
+
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(
+        default="/shared/public/models/Meta-Llama-3-8B-Instruct"
+    )
+
+
+@dataclass
+class DataArguments:
+    data_path: str = field(
+        default="sharegpt_clean.json",
+        metadata={"help": "Path to the training data."},
+    )
+    eval_data_path: str = field(
+        default=None, metadata={"help": "Path to the evaluation data."}
+    )
+    lazy_preprocess: bool = True
+
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    report_to: Optional[str] = None
+    optim: str = field(default="adamw_torch")
+    model_max_length: int = field(
+        default=2048,
+        metadata={
+            "help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
+        },
+    )
+    medusa_num_heads: int = field(
+        default=1,
+        metadata={"help": "Number of Medusa heads."},
+    )
+    medusa_num_layers: int = field(
+        default=1,
+        metadata={"help": "Number of layers for each Medusa head."},
+    )
+    medusa_heads_coefficient: float = field(
+        default=1.0,
+        metadata={"help": "Coefficient for the Medusa heads."},
+    )
+    medusa_decay_coefficient: float = field(
+        default=1.0,
+        metadata={"help": "Coefficient for the Medusa heads."},
+    )
+    medusa_scheduler: str = field(
+        default="constant",
+        metadata={"help": "Scheduler for the Medusa heads."},
+    )
+    medusa_lr_multiplier: float = field(
+        default=0.0,
+        metadata={"help": "Learning rate multiplier for the Medusa heads."},
+    )
+    medusa_return: bool = field(
+        default=False,
+        metadata={"help": "If return medusa heads predict only."},
+    )
+    medusa_only_heads: bool = field(
+        default=False,
+        metadata={
+            "help": "If train medusa heads only, default is False, the whole model will be trained"
+        },
+    )
+    with_liger: bool = field(
+        default=False,
+        metadata={"help": "If apply liger kernel to the model."},
+    )
+
+
+local_rank = None
+
+
+def rank0_print(*args):
+    if local_rank == 0:
+        print(*args)
+
+
+def safe_save_model_for_hf_trainer(trainer: transformers.Trainer, output_dir: str):
+    """
+    Save the model's state dictionary to a specified directory.
+
+    Args:
+        trainer (transformers.Trainer): The Hugging Face Trainer object.
+        output_dir (str): The directory where the model state dictionary will be saved.
+    """
+    state_dict = trainer.model.state_dict()
+    if trainer.args.should_save:
+        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
+        del state_dict
+        trainer._save(output_dir, state_dict=cpu_state_dict)  # noqa
+
+
+def preprocess(
+    sources,
+    tokenizer: transformers.PreTrainedTokenizer,
+) -> Dict:
+    """
+    Preprocesses conversation data and tokenizes it for model input.
+
+    Args:
+        sources: A list of conversation sources.
+        tokenizer (transformers.PreTrainedTokenizer): The tokenizer to use for tokenization.
+
+    Returns:
+        Dict: A dictionary containing tokenized inputs, labels, and attention mask.
+    """
+
+    # Apply prompt templates
+    conversations = []
+    prompts = []
+    # import pdb; pdb.set_trace()
+    for conversation in sources[:50]:
+        tokenizer_compatible_conv = [
+            {
+                "role": "user" if c["from"] == "human" else "assistant",
+                "content": c["value"],
+            }
+            for c in conversation["conversations"]
+        ]
+        prompt = tokenizer.apply_chat_template(
+            tokenizer_compatible_conv, tokenize=False
+        )
+        prompts.append(prompt)
+        conversations.append(tokenizer_compatible_conv)
+
+    # Tokenize conversations
+    encoding = tokenizer(
+        prompts,
+        return_tensors="pt",
+        padding="max_length",
+        truncation=True,
+        return_offsets_mapping=True,
+    )
+    # Set everything to be ignored, except the assistant part
+    targets = torch.full_like(encoding.input_ids, IGNORE_TOKEN_ID)
+    input_ids = encoding.input_ids
+
+    # Mask targets. Only compute loss on the assistant outputs.
+    for conv_index, (conversation, target, prompt) in enumerate(
+        zip(conversations, targets, prompts)
+    ):
+        # print(conv_index)
+        for turn in conversation:
+            if turn["role"] == "assistant":
+                content = turn["content"]
+                # Unfortunate strip() necessary because chat templates are doing the same.
+                start = prompt.index(content.strip())
+                # stop = start + len(content)
+                indices = []
+                for tok_index, (tok_start, tok_stop) in enumerate(
+                    encoding.offset_mapping[conv_index]
+                ):
+                    if tok_stop >= start or tok_start < tok_stop:
+                        indices.append(tok_index)
+                target[indices] = encoding.input_ids[conv_index][indices]
+
+    return dict(
+        input_ids=input_ids,
+        labels=targets,
+        attention_mask=input_ids.ne(tokenizer.pad_token_id),
+    )
+
+
+class SupervisedDataset(Dataset):
+    """Dataset for supervised fine-tuning.
+
+    Args:
+        raw_data (list): A list of raw data examples.
+        tokenizer (transformers.PreTrainedTokenizer): The tokenizer to use for data preprocessing.
+    """
+
+    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
+        super(SupervisedDataset, self).__init__()
+
+        rank0_print("Formatting inputs...")
+        sources = raw_data
+        data_dict = preprocess(sources, tokenizer)
+
+        self.input_ids = data_dict["input_ids"]
+        self.labels = data_dict["labels"]
+        self.attention_mask = data_dict["attention_mask"]
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return dict(
+            input_ids=self.input_ids[i],
+            labels=self.labels[i],
+            attention_mask=self.attention_mask[i],
+        )
+
+
+class LazySupervisedDataset(Dataset):
+    """Lazy dataset for supervised fine-tuning.
+
+    This dataset loads data on-the-fly when requested, which can be memory-efficient but slower.
+
+    Args:
+        raw_data (list): A list of raw data examples.
+        tokenizer (transformers.PreTrainedTokenizer): The tokenizer to use for data preprocessing.
+    """
+
+    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer):
+        super(LazySupervisedDataset, self).__init__()
+        self.tokenizer = tokenizer
+
+        rank0_print("Formatting inputs...Skip in lazy mode")
+        self.tokenizer = tokenizer
+        self.raw_data = raw_data
+        self.cached_data_dict = {}
+
+    def __len__(self):
+        return len(self.raw_data)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        if i in self.cached_data_dict:
+            return self.cached_data_dict[i]
+
+        ret = preprocess([self.raw_data[i]], self.tokenizer)
+        ret = dict(
+            input_ids=ret["input_ids"][0],
+            labels=ret["labels"][0],
+            attention_mask=ret["attention_mask"][0],
+        )
+        self.cached_data_dict[i] = ret
+
+        return ret
+
+
+def make_supervised_data_module(
+    tokenizer: transformers.PreTrainedTokenizer, data_args, test_size=0.05
+) -> Dict:
+    """Make dataset and collator for supervised fine-tuning.
+
+    Args:
+        tokenizer (transformers.PreTrainedTokenizer): The tokenizer to use for data preprocessing.
+        data_args: Data arguments.
+        test_size: evaluation data ratio (default: 0.05)
+
+    Returns:
+        dict: A dictionary containing train and eval datasets.
+    """
+    dataset_cls = (
+        LazySupervisedDataset if data_args.lazy_preprocess else SupervisedDataset
+    )
+    rank0_print("Loading data...")
+
+    # Load the entire dataset
+    train_json = json.load(open(data_args.data_path, "r"))
+
+    # Perform a train-test split based on test_size
+    train_data, eval_data = train_test_split(
+        train_json, test_size=test_size, random_state=42
+    )
+    # Create the train and eval datasets
+    train_dataset = dataset_cls(train_data, tokenizer=tokenizer)
+    eval_dataset = dataset_cls(eval_data, tokenizer=tokenizer)
+
+    return dict(train_dataset=train_dataset, eval_dataset=eval_dataset)
+
+
+def train():
+    global local_rank
+
+    parser = transformers.HfArgumentParser(
+        (ModelArguments, DataArguments, TrainingArguments)
+    )
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    local_rank = training_args.local_rank
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        model_max_length=training_args.model_max_length,
+        padding_side="right",
+        use_fast=True,
+    )
+    tokenizer.pad_token = tokenizer.unk_token
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # Making sure the tokenizer works before loading the model.
+    print(tokenizer(["This is a test", "secondary"], padding=True))
+    print(
+        tokenizer.apply_chat_template([{"role": "user", "content": "This is a test"}])
+    )
+
+    # Load model and tokenizer
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        # config=config,
+        cache_dir=training_args.cache_dir,
+        torch_dtype=torch.bfloat16,
+    )
+
+    if training_args.with_liger is True:
+        apply_liger_kernel_to_llama()
+
+    # Freeze the base model
+    for param in model.base_model.parameters():
+        param.requires_grad = False
+
+    add_medusa_heads(
+        model,
+        training_args.medusa_num_heads,
+        training_args.medusa_num_layers,
+        training_args.medusa_return,
+        training_args.medusa_only_heads,
+        training_args.with_liger,
+    )
+    # Format output dir
+    training_args.output_dir = f"{training_args.output_dir}_medusa_mlp_{model_args.model_name_or_path.split('/')[-1]}_medusa_{training_args.medusa_num_heads}_lr_{training_args.learning_rate}_layers_{training_args.medusa_num_layers}"
+
+    # Load data
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+
+    # Start trainner
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        callbacks=[EfficiencyCallback()],
+        **data_module,
+    )
+
+    if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
+        trainer.train(resume_from_checkpoint=True)
+    else:
+        trainer.train()
+
+
+if __name__ == "__main__":
+    train()
