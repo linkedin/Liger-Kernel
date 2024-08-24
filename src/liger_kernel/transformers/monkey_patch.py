6782a77f65303c0f59e38d282f390380d0c0ebb1
Chiwan Park <chiwanpark@hotmail.com>
2024-08-25 01:41:16 +0900

    Add monkey patch for Qwen2 models (#69)
    ## Summary
    This PR adds a monkey patch script for Qwen2 models. I've copied the
    monkey patch script for Mixtral because the Qwen2 models have the same
    structure of Mixtral. Also, I've checked the `forward` method of
    `Qwen2ForCausalLM` class
    ([link](https://github.com/huggingface/transformers/blob/main/src/transformers/models/qwen2/modeling_qwen2.py#L1058))
    to apply the fused linear cross entropy loss.
    
    <!---
    ## Details
    This is an optional section; is there anything specific that reviewers
    should be aware of?
    --->
    
    ## Testing Done
    <!--- This is a required section; please describe how this change was
    tested. --->
    
    <!-- 
    Complete the following tasks before sending your PR, and replace `[ ]`
    with
    `[x]` to indicate you have done them. 
    -->
    
    - [x] run `make test` to ensure correctness
    - [x] run `make checkstyle` to ensure code style
    - [x] run `make test-convergence` to ensure convergence
    
    ```zsh
    ❯ make test
    pytest --disable-warnings test/ --ignore=test/convergence
    ================================================ test session starts =================================================
    platform linux -- Python 3.10.12, pytest-8.3.2, pluggy-1.5.0
    rootdir: /data/ovis_915/bell/Liger-Kernel.git
    plugins: anyio-4.4.0
    collected 114 items                                                                                                  
    
    test/transformers/test_cross_entropy.py ..........................................................             [ 50%]
    test/transformers/test_fused_linear_cross_entropy.py ......                                                    [ 56%]
    test/transformers/test_geglu.py ........                                                                       [ 63%]
    test/transformers/test_rms_norm.py ................                                                            [ 77%]
    test/transformers/test_rope.py ............                                                                    [ 87%]
    test/transformers/test_swiglu.py ........                                                                      [ 94%]
    test/transformers/test_trainer_integration.py ...                                                              [ 97%]
    test/transformers/test_transformers_monkey_patch.py .                                                          [ 98%]
    test/triton/test_triton_monkey_patch.py ..                                                                     [100%]
    
    =========================================== 114 passed in 62.15s (0:01:02) ===========================================
    ```
    
    ```zsh
    ❯ make checkstyle 
    flake8 .; flake8_status=$?; \
    isort .; isort_status=$?; \
    black .; black_status=$?; \
    if [ $flake8_status -ne 0 ] || [ $isort_status -ne 0 ] || [ $black_status -ne 0 ]; then \
            exit 1; \
    fi
    Skipped 1 files
    All done! ✨ 🍰 ✨
    52 files left unchanged.
    ```
    
    ```zsh
    ❯ make test-convergence 
    HF_DATASETS_OFFLINE=1 pytest --disable-warnings test/convergence
    ================================================ test session starts =================================================
    platform linux -- Python 3.10.12, pytest-8.3.2, pluggy-1.5.0
    rootdir: /data/ovis_915/bell/Liger-Kernel.git
    plugins: anyio-4.4.0
    collected 8 items                                                                                                    
    
    test/convergence/test_mini_models.py ......                                                                    [ 75%]
    test/convergence/test_mini_models_no_logits.py ..                                                              [100%]
    
    ================================================= 8 passed in 55.44s =================================================
    
    ---------
    
    Co-authored-by: Shao Tang <tangshao28@gmail.com>
    Co-authored-by: Byron Hsu <byronhsu1230@gmail.com>

--- src/liger_kernel/transformers/monkey_patch.py
+++ src/liger_kernel/transformers/monkey_patch.py
@@ -1,6 +1,7 @@
 from liger_kernel.transformers.cross_entropy import LigerCrossEntropyLoss
 from liger_kernel.transformers.geglu import LigerGEGLUMLP
 from liger_kernel.transformers.model.llama import lce_forward
+from liger_kernel.transformers.model.qwen2 import lce_forward as qwen2_lce_forward
 from liger_kernel.transformers.rms_norm import LigerRMSNorm
 from liger_kernel.transformers.rope import liger_rotary_pos_emb
 from liger_kernel.transformers.swiglu import LigerBlockSparseTop2MLP, LigerSwiGLUMLP
@@ -128,3 +129,41 @@ def apply_liger_kernel_to_gemma(
         modeling_gemma.CrossEntropyLoss = LigerCrossEntropyLoss
     if geglu:
         modeling_gemma.GemmaMLP = LigerGEGLUMLP
+
+
+def apply_liger_kernel_to_qwen2(
+    rope: bool = True,
+    cross_entropy: bool = False,
+    fused_linear_cross_entropy: bool = True,
+    rms_norm: bool = True,
+    swiglu: bool = True,
+) -> None:
+    """
+    Apply Liger kernels to replace original implementation in HuggingFace Qwen2 models
+
+    Args:
+        rope (bool): Whether to apply Liger's rotary position embedding. Default is True.
+        cross_entropy (bool): Whether to apply Liger's cross entropy loss. Default is False.
+        fused_linear_cross_entropy (bool):
+            Whether to apply Liger's fused lienar cross entropy loss. Default is True.
+            `cross_entropy` and `fused_linear_cross_entropy` cannot both be True.
+            If `fused_linear_cross_entropy` is True, the logits will not be materialized but more memory efficient.
+        rms_norm (bool): Whether to apply Liger's RMSNorm. Default is True.
+        swiglu (bool): Whether to apply Liger's SwiGLU MLP. Default is True.
+    """
+    assert not (
+        cross_entropy and fused_linear_cross_entropy
+    ), "cross_entropy and fused_linear_cross_entropy cannot both be True."
+
+    from transformers.models.qwen2 import modeling_qwen2
+
+    if rope:
+        modeling_qwen2.apply_rotary_pos_emb = liger_rotary_pos_emb
+    if rms_norm:
+        modeling_qwen2.Qwen2RMSNorm = LigerRMSNorm
+    if cross_entropy:
+        modeling_qwen2.CrossEntropyLoss = LigerCrossEntropyLoss
+    if fused_linear_cross_entropy:
+        modeling_qwen2.Qwen2ForCausalLM.forward = qwen2_lce_forward
+    if swiglu:
+        modeling_qwen2.Qwen2MLP = LigerSwiGLUMLP
