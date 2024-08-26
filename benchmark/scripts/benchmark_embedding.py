a1a56a855c224f7364e825b81d3dcb31722449cc
Steven Shimizu <shimizust@gmail.com>
2024-08-26 19:05:11 +0000

    Start of benchmark refactor
    add'l stuff
    
    Not working
    
    Somewhat working
    
    Working benchmarking script
    
    Fixed script for overwriting values
    
    Updated swiglu
    
    Updated benchmark_rope
    
    Updated benchmark_rms_norm
    
    Updated flce
    
    Updated geglu
    
    Updated embedding
    
    Updated layernorm
    
    working notebook
    
    Cleared outputs
    
    Reran for latest liger version, switched to quantiles
    
    Fixe checkstyle
    
    Added instructions
    
    fixed typo

---
+++ benchmark/scripts/benchmark_embedding.py
@@ -0,0 +1,130 @@
+import torch
+import triton
+from torch.nn import Embedding
+from utils import (
+    QUANTILES,
+    SingleBenchmarkRunInput,
+    SingleBenchmarkRunOutput,
+    _test_memory,
+    parse_benchmark_script_args,
+    run_benchmarks,
+)
+
+from liger_kernel.transformers.experimental.embedding import LigerEmbedding
+
+# NOTE: For torch compile, we will just use default inductor settings. No further customization
+# is needed.
+
+
+def bench_speed_embedding(input: SingleBenchmarkRunInput) -> SingleBenchmarkRunOutput:
+    V = input.x
+    provider = input.kernel_provider
+    mode = input.kernel_operation_mode
+
+    B = input.extra_benchmark_config["B"]
+    T = input.extra_benchmark_config["T"]
+    D = input.extra_benchmark_config["D"]
+    dtype = input.extra_benchmark_config["dtype"]
+
+    device = "cuda"
+
+    torch_emb = Embedding(V, D).to(device).to(dtype)
+    liger_emb = LigerEmbedding(V, D).to(device).to(dtype)
+    torch_compile_emb = torch.compile(torch_emb)
+
+    input_ids = torch.randint(0, V, (B, T), device=device)
+
+    def fwd():
+        if provider == "liger":
+            return liger_emb(input_ids)
+        elif provider == "torch_compile":
+            return torch_compile_emb(input_ids)
+        else:
+            return torch_emb(input_ids)
+
+    def full():
+        output = fwd()
+        output.backward(torch.randn_like(output))
+
+    if mode == "forward":
+        ms_50, ms_20, ms_80 = triton.testing.do_bench(fwd, quantiles=QUANTILES, rep=100)
+    elif mode == "full":
+        ms_50, ms_20, ms_80 = triton.testing.do_bench(
+            full, quantiles=QUANTILES, rep=100
+        )
+    return SingleBenchmarkRunOutput(
+        y_20=ms_20,
+        y_50=ms_50,
+        y_80=ms_80,
+    )
+
+
+def bench_memory_embedding(input: SingleBenchmarkRunInput) -> SingleBenchmarkRunOutput:
+    V = input.x
+    provider = input.kernel_provider
+
+    B = input.extra_benchmark_config["B"]
+    T = input.extra_benchmark_config["T"]
+    D = input.extra_benchmark_config["D"]
+    dtype = input.extra_benchmark_config["dtype"]
+
+    device = "cuda"
+
+    torch_emb = Embedding(V, D).to(device).to(dtype)
+    liger_emb = LigerEmbedding(V, D).to(device).to(dtype)
+    torch_compile_emb = torch.compile(torch_emb)
+
+    input_ids = torch.randint(0, V, (B, T), device=device)
+
+    def fwd():
+        if provider == "liger":
+            return liger_emb(input_ids)
+        elif provider == "torch_compile":
+            return torch_compile_emb(input_ids)
+        else:
+            return torch_emb(input_ids)
+
+    def full():
+        output = fwd()
+        output.backward(torch.randn_like(output))
+
+    mem_50, mem_20, mem_80 = _test_memory(full, quantiles=QUANTILES)
+    return SingleBenchmarkRunOutput(
+        y_20=mem_20,
+        y_50=mem_50,
+        y_80=mem_80,
+    )
+
+
+if __name__ == "__main__":
+    args = parse_benchmark_script_args()
+
+    common_configs = {
+        "kernel_name": "embedding",
+        "x_name": "V",
+        "x_label": "embedding dimension",
+        "x_values": [2**i for i in range(10, 18)],
+        "kernel_providers": ["liger", "huggingface", "torch_compile"],
+        "extra_benchmark_configs": [
+            # BERT
+            {"B": 32, "T": 512, "D": 768, "dtype": torch.float32},
+            # Llama
+            {"B": 8, "T": 2048, "D": 4096, "dtype": torch.float32},
+        ],
+        "overwrite": args.overwrite,
+    }
+
+    run_benchmarks(
+        bench_test_fn=bench_speed_embedding,
+        kernel_operation_modes=["forward", "full"],
+        metric_name="speed",
+        metric_unit="ms",
+        **common_configs
+    )
+    run_benchmarks(
+        bench_test_fn=bench_memory_embedding,
+        kernel_operation_modes=["full"],
+        metric_name="memory",
+        metric_unit="MB",
+        **common_configs
+    )
---
+++ benchmark/scripts/benchmark_rms_norm.py
@@ -0,0 +1,160 @@
+import torch
+import torch.nn as nn
+import triton
+from utils import (
+    QUANTILES,
+    SingleBenchmarkRunInput,
+    SingleBenchmarkRunOutput,
+    _test_memory,
+    parse_benchmark_script_args,
+    run_benchmarks,
+)
+
+from liger_kernel.transformers.rms_norm import LigerRMSNorm
+
+
+class LlamaRMSNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        """
+        LlamaRMSNorm is equivalent to T5LayerNorm
+        """
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+        input_dtype = hidden_states.dtype
+        hidden_states = hidden_states.to(torch.float32)
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+        return self.weight * hidden_states.to(input_dtype)
+
+
+def bench_speed_rms_norm(input: SingleBenchmarkRunInput) -> SingleBenchmarkRunOutput:
+    N = input.x
+    provider = input.kernel_provider
+    mode = input.kernel_operation_mode
+
+    extra_benchmark_config = input.extra_benchmark_config
+    M = extra_benchmark_config["M"]
+    eps = extra_benchmark_config["eps"]
+    dtype = extra_benchmark_config["dtype"]
+
+    x_shape = (M, N)
+
+    triton_rms = LigerRMSNorm(hidden_size=N, eps=eps).to("cuda")
+    llama_rms = LlamaRMSNorm(hidden_size=N, eps=eps).to("cuda")
+
+    x = torch.randn(x_shape, dtype=dtype, device="cuda")
+    dy = torch.randn_like(x)
+    x.requires_grad_(True)
+
+    # utility functions
+
+    def y_fwd():
+        if provider == "liger":
+            return triton_rms(x)
+
+        if provider == "huggingface":
+            return llama_rms(x)
+
+    if mode == "forward":
+        ms_50, ms_20, ms_80 = triton.testing.do_bench(
+            y_fwd,
+            grad_to_none=[x],
+            rep=500,
+            quantiles=QUANTILES,
+        )
+    elif mode == "backward":
+        y = y_fwd()
+        ms_50, ms_20, ms_80 = triton.testing.do_bench(
+            lambda: y.backward(dy, retain_graph=True),
+            grad_to_none=[x],
+            rep=500,
+            quantiles=QUANTILES,
+        )
+    elif mode == "full":
+
+        def full():
+            y = y_fwd()
+            y.backward(dy, retain_graph=True)
+
+        ms_50, ms_20, ms_80 = triton.testing.do_bench(
+            full,
+            grad_to_none=[x],
+            rep=500,
+            quantiles=QUANTILES,
+        )
+
+    return SingleBenchmarkRunOutput(
+        y_20=ms_20,
+        y_50=ms_50,
+        y_80=ms_80,
+    )
+
+
+def bench_memory_rms_norm(input: SingleBenchmarkRunInput) -> SingleBenchmarkRunOutput:
+    N = input.x
+    provider = input.kernel_provider
+
+    extra_benchmark_config = input.extra_benchmark_config
+    M = extra_benchmark_config["M"]
+    eps = extra_benchmark_config["eps"]
+    dtype = extra_benchmark_config["dtype"]
+
+    x_shape = (M, N)
+
+    triton_rms = LigerRMSNorm(hidden_size=N, eps=eps).to("cuda")
+    llama_rms = LlamaRMSNorm(hidden_size=N, eps=eps).to("cuda")
+
+    x = torch.randn(x_shape, dtype=dtype, device="cuda")
+    dy = torch.randn_like(x)
+    x.requires_grad_(True)
+
+    # utility functions
+    def y_fwd():
+        if provider == "liger":
+            return triton_rms(x)
+        if provider == "huggingface":
+            return llama_rms(x)
+
+    def full():
+        y = y_fwd()
+        y.backward(dy, retain_graph=True)
+
+    mem_50, mem_20, mem_80 = _test_memory(full, quantiles=QUANTILES)
+
+    return SingleBenchmarkRunOutput(
+        y_20=mem_20,
+        y_50=mem_50,
+        y_80=mem_80,
+    )
+
+
+if __name__ == "__main__":
+    args = parse_benchmark_script_args()
+
+    common_configs = {
+        "kernel_name": "rms_norm",
+        "x_name": "H",
+        "x_label": "hidden size",
+        "x_values": [2**i for i in range(10, 16)],
+        "kernel_providers": ["liger", "huggingface"],
+        "extra_benchmark_configs": [{"M": 2048, "dtype": torch.bfloat16, "eps": 1e-6}],
+        "overwrite": args.overwrite,
+    }
+
+    run_benchmarks(
+        bench_test_fn=bench_speed_rms_norm,
+        kernel_operation_modes=["forward", "full", "backward"],
+        metric_name="speed",
+        metric_unit="ms",
+        **common_configs
+    )
+    run_benchmarks(
+        bench_test_fn=bench_memory_rms_norm,
+        kernel_operation_modes=["full"],
+        metric_name="memory",
+        metric_unit="MB",
+        **common_configs
+    )
