2d5ffc2517bcc2cf8a8f869a630fd37cbcee4ce0
S1ro1 <matej.sirovatka@gmail.com>
2024-08-29 23:00:34 +0000

    Feat: fused linear cross entropy tests

--- test/transformers/test_fused_linear_cross_entropy.py
+++ test/transformers/test_fused_linear_cross_entropy.py
@@ -1,4 +1,4 @@
-from test.utils import assert_verbose_allclose, set_seed
+# from test.utils import assert_verbose_allclose, set_seed
 
 import pytest
 import torch
@@ -7,8 +7,13 @@ from liger_kernel.transformers.fused_linear_cross_entropy import (
     LigerFusedLinearCrossEntropyLoss,
 )
 
+from liger_kernel.transformers.functional import liger_fused_linear_cross_entropy
+from liger_kernel.ops.fused_linear_cross_entropy import (
+    LigerFusedLinearCrossEntropyFunction,
+)
+
 # set random seed globally
-set_seed()
+# set_seed()
 
 
 class TorchLMHeadCE(torch.nn.Module):
@@ -131,3 +136,47 @@ def test_correctness(B, T, H, V, scalar, dtype, bias, atol, rtol):
             atol=atol,
             rtol=rtol,
         )
+
+
+@pytest.mark.parametrize(
+    "B, T, H, V",
+    [
+        # (2, 4, 512, 512),  # The test does not work on some CI GPUs. Issue #160
+        (8, 2048, 4096, 32000),  # llama2, mistral
+        # Comment out to speed up testing
+        # (4, 2048, 4096, 128256),  # llama3 8B
+        # (4, 1024, 8192, 128256),  # llama3 70B
+        (4, 423, 8192, 32000),  # random shape
+    ],
+)
+@pytest.mark.parametrize(
+    "scalar, dtype, atol, rtol",
+    [
+        (1.0, torch.bfloat16, 5e-3, 5e-2),
+        (1.0, torch.float32, 1e-5, 5e-4),
+    ],
+)
+@pytest.mark.parametrize("bias", [True, False])
+def test_correctness_functional(B, T, H, V, scalar, dtype, bias, atol, rtol):
+    device = "cuda"
+
+    _input = torch.randn(B * T, H, device=device, dtype=dtype) * scalar
+    x1 = _input.detach().clone().requires_grad_(True)
+    x2 = _input.detach().clone().requires_grad_(True)
+
+    target = torch.randint(0, V, (B * T,), device=device, dtype=torch.long)
+
+    weight = torch.randn(V, H, device=device, dtype=dtype)
+    bias = torch.randn(V, device=device, dtype=dtype) if bias else None
+
+    y1 = liger_fused_linear_cross_entropy(x1, weight, target, bias)
+    y2 = LigerFusedLinearCrossEntropyFunction.apply(x2, weight, target, bias)
+
+    assert torch.allclose(y1, y2, atol=atol, rtol=rtol)
+
+    grad_output = torch.randn_like(y1)
+
+    y1.backward(grad_output)
+    y2.backward(grad_output)
+
+    assert torch.allclose(x1.grad, x2.grad, atol=atol, rtol=rtol)
