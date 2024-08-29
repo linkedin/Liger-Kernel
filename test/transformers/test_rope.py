0b56a235bc4e4495dabccfbbf46b4c9352e42f5f
S1ro1 <matej.sirovatka@gmail.com>
2024-08-29 22:12:58 +0000

    Feat: rope tests

--- test/transformers/test_rope.py
+++ test/transformers/test_rope.py
@@ -6,6 +6,8 @@ from transformers.models.llama.modeling_llama import (
 )
 
 from liger_kernel.transformers.rope import liger_rotary_pos_emb
+from liger_kernel.ops.rope import LigerRopeFunction
+from liger_kernel.transformers.functional import liger_rope
 
 SLEEP_SECONDS = 0.1
 
@@ -35,7 +37,6 @@ SLEEP_SECONDS = 0.1
 def test_correctness(
     bsz, seq_len, num_q_heads, num_kv_heads, head_dim, dtype, atol, rtol
 ):
-
     rotary_emb = LlamaRotaryEmbedding(head_dim, device="cuda")
 
     _tensor_q = (
@@ -66,9 +67,10 @@ def test_correctness(
     assert torch.allclose(hf_k, tt_k, atol=atol, rtol=rtol)
 
     # validate backward pass
-    dq, dk = torch.randn_like(hf_q, device="cuda"), torch.randn_like(
-        hf_k, device="cuda"
-    ).to(dtype)
+    dq, dk = (
+        torch.randn_like(hf_q, device="cuda"),
+        torch.randn_like(hf_k, device="cuda").to(dtype),
+    )
 
     q1_grad, k1_grad = torch.autograd.grad(
         (hf_q, hf_k), (q1, k1), (dq, dk), allow_unused=True
@@ -79,3 +81,51 @@ def test_correctness(
 
     assert torch.allclose(q1_grad, q2_grad, atol=atol, rtol=rtol)
     assert torch.allclose(k1_grad, k2_grad, atol=atol, rtol=rtol)
+
+
+@pytest.mark.parametrize(
+    "bsz, seq_len, num_q_heads, num_kv_heads, head_dim",
+    [
+        (1, 128, 32, 32, 64),
+        (2, 128, 32, 32, 64),
+        # different q/k heads
+        (1, 128, 32, 8, 64),
+        (2, 128, 32, 8, 64),
+        # weird shapes
+        # HuggingFace llama/mistral source code doesn't support odd head dimension
+        # so we don't test it here
+        (3, 423, 73, 213, 92),
+        (3, 423, 73, 155, 92),
+    ],
+)
+@pytest.mark.parametrize(
+    "dtype, atol, rtol",
+    [
+        (torch.float32, 1e-5, 1e-5),
+        (torch.bfloat16, 1e-1, 1e-5),
+    ],
+)
+def test_functional_correctness(
+    bsz, seq_len, num_q_heads, num_kv_heads, head_dim, dtype, atol, rtol
+):
+    _q = torch.randn((bsz, num_q_heads, seq_len, head_dim), device="cuda", dtype=dtype)
+    _k = torch.randn((bsz, num_kv_heads, seq_len, head_dim), device="cuda", dtype=dtype)
+
+    q1 = _q.clone().requires_grad_(True)
+    q2 = _q.clone().requires_grad_(True)
+
+    k1 = _k.clone().requires_grad_(True)
+    k2 = _k.clone().requires_grad_(True)
+
+    rotary_emb = LlamaRotaryEmbedding(head_dim, device="cuda")
+
+    pos_ids = torch.arange(seq_len, device="cuda", dtype=torch.long).unsqueeze(0)
+    cos, sin = rotary_emb(k1, pos_ids)
+
+    functional_q, functional_k = liger_rope(q1, k1, cos, sin)
+    class_q, class_k = LigerRopeFunction.apply(q2, k2, cos, sin)
+
+    assert torch.allclose(functional_q, class_q, atol=atol, rtol=rtol)
+    assert torch.allclose(functional_k, class_k, atol=atol, rtol=rtol)
+
+    #TODO: implement backwards tests
