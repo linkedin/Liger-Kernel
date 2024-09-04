1da9aa1fe48510adb924fcf24840a66964f9e68b
Austin Liu <austin362667@gmail.com>
2024-09-05 02:44:21 +0800

    Uplift imports structure
    Signed-off-by: Austin Liu <austin362667@gmail.com>
    
    format
    
    Signed-off-by: Austin Liu <austin362667@gmail.com>
    
    lint
    
    Signed-off-by: Austin Liu <austin362667@gmail.com>

--- src/liger_kernel/transformers/__init__.py
+++ src/liger_kernel/transformers/__init__.py
@@ -10,3 +10,23 @@ from liger_kernel.transformers.monkey_patch import (  # noqa: F401
     apply_liger_kernel_to_phi3,
     apply_liger_kernel_to_qwen2,
 )
+
+from .cross_entropy import LigerCrossEntropyLoss
+from .fused_linear_cross_entropy import LigerFusedLinearCrossEntropyLoss
+from .geglu import LigerGEGLUMLP
+from .layer_norm import LigerLayerNorm
+from .rms_norm import LigerRMSNorm
+from .rope import liger_rotary_pos_emb
+from .swiglu import LigerBlockSparseTop2MLP, LigerPhi3SwiGLUMLP, LigerSwiGLUMLP
+
+__all__ = [
+    "LigerCrossEntropyLoss",
+    "LigerFusedLinearCrossEntropyLoss",
+    "LigerGEGLUMLP",
+    "LigerLayerNorm",
+    "LigerRMSNorm",
+    "liger_rotary_pos_emb",
+    "LigerBlockSparseTop2MLP",
+    "LigerPhi3SwiGLUMLP",
+    "LigerSwiGLUMLP",
+]
