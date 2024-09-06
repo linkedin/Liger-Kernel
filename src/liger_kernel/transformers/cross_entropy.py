a036c15683d9e36243d0834f8c7d96d406ff65a3
Tcc0403 <tcc32789@gmail.com>
2024-09-07 04:58:09 +0800

    Fix the assertion test for label_smoothing

--- src/liger_kernel/transformers/cross_entropy.py
+++ src/liger_kernel/transformers/cross_entropy.py
@@ -6,8 +6,8 @@ from liger_kernel.ops.cross_entropy import LigerCrossEntropyFunction
 class LigerCrossEntropyLoss(CrossEntropyLoss):
     def __init__(self, *args, **kwargs):
         super(LigerCrossEntropyLoss, self).__init__(*args, **kwargs)
-        assert (self.label_smoothing > 0) and (
-            self.label_smoothing < 1
+        assert (self.label_smoothing >= 0) and (
+            self.label_smoothing <= 1
         ), f"label_smoothing must be between 0.0 and 1.0. Got: {self.label_smoothing}"
 
     def forward(self, _input, target):
