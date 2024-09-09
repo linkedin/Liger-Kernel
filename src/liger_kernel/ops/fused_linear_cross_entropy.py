fix gradient computation for flce

src/liger_kernel/ops/fused_linear_cross_entropy.py
@@ -101,10 +101,15 @@ def fused_linear_cross_entropy_forward(
         # additionally, since we are chunking the inputs, observe that the loss and gradients are calculated only
         # on `n_non_ignore` tokens. However, the gradient of the input should be calculated for all tokens.
         # Thus, we need an additional scaling factor of (n_non_ignore/total_n_non_ignore) to scale the gradients.
-        loss_1d[start_idx:end_idx] = loss_1d_slice * n_non_ignore
-        grad_logits_chunk = logits_chunk * (
-            n_non_ignore / total_n_non_ignore
-        )  # chunk_size x V
+        
+        if reduction == "mean":
+            alpha = n_non_ignore / total_n_non_ignore if total_n_non_ignore > 0 else 0.0
+        else:
+            alpha = 1.0
+
+        loss_1d[start_idx:end_idx] = loss_1d_slice * alpha
+        grad_logits_chunk = logits_chunk * alpha  # chunk_size x V
+
         grad_input[start_idx:end_idx] = grad_logits_chunk @ weight
 
         if grad_weight is not None:
@@ -113,7 +118,7 @@ def fused_linear_cross_entropy_forward(
                 mat1=logits_chunk.t(),
                 mat2=_input_chunk,
                 out=grad_weight,
-                alpha=n_non_ignore / total_n_non_ignore,
+                alpha=alpha,
                 beta=1.0,
             )
 
@@ -122,10 +127,10 @@ def fused_linear_cross_entropy_forward(
                 input=grad_bias,
                 other=logits_chunk.sum(dim=0),
                 out=grad_bias,
-                alpha=n_non_ignore / total_n_non_ignore,
+                alpha=alpha,
             )
 
-    loss = torch.sum(loss_1d) / total_n_non_ignore
+    loss = torch.sum(loss_1d)
     return loss, grad_input, grad_weight, grad_bias
 
 

test/transformers/test_fused_linear_cross_entropy.py
@@ -32,13 +32,14 @@ class TorchLMHeadCE(torch.nn.Module):
         bias: bool = False,
         ignore_index: int = -100,
         label_smoothing: float = 0.0,
+        reduction: str = "mean",
     ):
         super().__init__()
         self.lin = torch.nn.Linear(
             in_features=H, out_features=V, bias=bias, dtype=dtype
         )
         self.ce_loss = torch.nn.CrossEntropyLoss(
-            ignore_index=ignore_index, reduction="mean", label_smoothing=label_smoothing
+            ignore_index=ignore_index, reduction=reduction, label_smoothing=label_smoothing
         )
 
     def forward(self, x, y):
@@ -55,6 +56,7 @@ class LigerLMHeadCE(torch.nn.Module):
         bias: bool = False,
         ignore_index: int = -100,
         label_smoothing: float = 0.0,
+        reduction: str = "mean",
     ):
         super().__init__()
         self.lin = torch.nn.Linear(
@@ -62,7 +64,7 @@ class LigerLMHeadCE(torch.nn.Module):
         )
         self.ce_loss = LigerFusedLinearCrossEntropyLoss(
             ignore_index=ignore_index,
-            reduction="mean",
+            reduction=reduction,
             label_smoothing=label_smoothing,
         )
 
@@ -95,13 +97,14 @@ class LigerLMHeadCE(torch.nn.Module):
 )
 @pytest.mark.parametrize("bias", [True, False])
 @pytest.mark.parametrize("label_smoothing", [0, 0.1])
-def test_correctness(B, T, H, V, scalar, dtype, bias, label_smoothing, atol, rtol):
+@pytest.mark.parametrize("reduction", ["sum", "mean"])
+def test_correctness(B, T, H, V, scalar, dtype, bias, label_smoothing, reduction, atol, rtol):
     device = "cuda"
     torch_lm_head_ce = TorchLMHeadCE(
-        H=H, V=V, bias=bias, label_smoothing=label_smoothing, dtype=dtype
+        H=H, V=V, bias=bias, label_smoothing=label_smoothing, reduction=reduction, dtype=dtype
     ).to(device)
     liger_lm_head_ce = LigerLMHeadCE(
-        H=H, V=V, bias=bias, label_smoothing=label_smoothing, dtype=dtype
+        H=H, V=V, bias=bias, label_smoothing=label_smoothing, reduction=reduction, dtype=dtype
     ).to(device)
 
     # init the linear in all CEs with the same weights