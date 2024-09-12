7dc82f6ff01135455bfcd21880c45ce2da942915
Steven Shimizu <shimizust@gmail.com>
2024-09-12 17:16:35 +0000

    Re-export generic apply functions to facilitate later internal refactoring

--- src/liger_kernel/transformers/__init__.py
+++ src/liger_kernel/transformers/__init__.py
@@ -16,6 +16,8 @@ from liger_kernel.transformers.monkey_patch import (  # noqa: F401
     apply_liger_kernel_to_phi3,
     apply_liger_kernel_to_qwen2,
     apply_liger_kernel_to_qwen2_vl,
+    _apply_liger_kernel,
+    _apply_liger_kernel_to_instance,
 )
 from liger_kernel.transformers.rms_norm import LigerRMSNorm  # noqa: F401
 from liger_kernel.transformers.rope import liger_rotary_pos_emb  # noqa: F401
