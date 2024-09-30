2c57cd3d7d0731bee0c1ef431394650650ebc590
Tyler Romero <tyler.alexander.romero@gmail.com>
2024-09-29 21:33:14 -0700

    Checkstyle

--- test/transformers/test_monkey_patch.py
+++ test/transformers/test_monkey_patch.py
@@ -1,5 +1,6 @@
 import inspect
 from inspect import signature
+from test.utils import revert_liger_kernel_to_mllama
 from unittest.mock import MagicMock, Mock, patch
 
 import pytest
@@ -21,7 +22,6 @@ from liger_kernel.transformers.monkey_patch import (
     _apply_liger_kernel,
     _apply_liger_kernel_to_instance,
 )
-from test.utils import revert_liger_kernel_to_mllama
 
 
 def test_import_from_root():
