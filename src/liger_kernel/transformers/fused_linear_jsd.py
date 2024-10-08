Fix formula in fl_jsd

src/liger_kernel/transformers/fused_linear_jsd.py
@@ -21,6 +21,6 @@ class LigerFusedLinearJSD(nn.Module):
             student_weight,
             teacher_input,
             teacher_weight,
-            jsd_beta=self.jsd_beta,
-            temperature=self.temperature,
+            self.jsd_beta,
+            self.temperature,
         )

test/transformers/test_fused_linear_jsd.py
@@ -11,7 +11,7 @@ from liger_kernel.transformers.fused_linear_jsd import LigerFusedLinearJSD
 set_seed(42)
 
 
-class TorchLMHeadCE(torch.nn.Module):
+class TorchLMHeadJSD(torch.nn.Module):
     """Ground truth implementation of the linear fused with torch based jsd loss.
 
     :param H: hidden size
@@ -29,32 +29,39 @@ class TorchLMHeadCE(torch.nn.Module):
         super().__init__()
         self.student_lin = torch.nn.Linear(in_features=H, out_features=V, dtype=dtype)
         self.teacher_lin = torch.nn.Linear(in_features=H, out_features=V, dtype=dtype)
-        self.jsd = TorchJSD()
+        self.jsd = TorchJSD(beta, dtype=dtype)
         self.temperature = temperature
-        self.beta = beta
 
     def forward(self, student_input, teacher_input):
         student_logits = self.student_lin(student_input)
-        teacher_logits = self.teacher_lin_lin(teacher_input)
+        teacher_logits = self.teacher_lin(teacher_input)
         student_prob = torch.log_softmax(student_logits / self.temperature, dim=-1)
         teacher_prob = torch.log_softmax(teacher_logits / self.temperature, dim=-1)
 
         return self.jsd(student_prob, teacher_prob)
 
 
-class LigerLMHeadCE(torch.nn.Module):
+class LigerLMHeadJSD(torch.nn.Module):
     def __init__(
         self,
         H: int,
         V: int,
         dtype: torch.dtype,
+        temperature: float = 1.0,
+        beta: float = 0.5,
     ):
         super().__init__()
-        self.lin = torch.nn.Linear(in_features=H, out_features=V, dtype=dtype)
-        self.ce_loss = LigerFusedLinearJSD()
+        self.student_lin = torch.nn.Linear(in_features=H, out_features=V, dtype=dtype)
+        self.teacher_lin = torch.nn.Linear(in_features=H, out_features=V, dtype=dtype)
+        self.fused_jsd = LigerFusedLinearJSD(beta, temperature)
 
-    def forward(self, x, y):
-        return self.ce_loss(self.lin.weight, x, y, self.lin.bias)
+    def forward(self, student_input, teacher_input):
+        return self.fused_jsd(
+            student_input,
+            self.student_lin.weight,
+            teacher_input,
+            self.teacher_lin.weight,
+        )
 
 
 #############################################################################
@@ -65,64 +72,55 @@ class LigerLMHeadCE(torch.nn.Module):
 @pytest.mark.parametrize(
     "B, T, H, V",
     [
-        # (2, 4, 512, 512),  # The test does not work on some CI GPUs. Issue #160
-        (8, 2048, 4096, 32000),  # llama2, mistral
+        (2, 4, 512, 512),  # The test does not work on some CI GPUs. Issue #160
+        # (8, 2048, 4096, 32000),  # llama2, mistral
         # Comment out to speed up testing
         # (4, 2048, 4096, 128256),  # llama3 8B
         # (4, 1024, 8192, 128256),  # llama3 70B
-        (4, 423, 8192, 32000),  # random shape
+        # (4, 423, 8192, 32000),  # random shape
     ],
 )
 @pytest.mark.parametrize(
-    "reduction, scalar, dtype, atol, rtol",
+    "scalar, dtype, atol, rtol",
     [
-        ("mean", 1.0, torch.bfloat16, 5e-3, 5e-2),
-        ("mean", 1.0, torch.float32, 1e-5, 5e-4),
-        ("sum", 1.0, torch.bfloat16, 5e-0, 5e1),
-        ("sum", 1.0, torch.float32, 1e-3, 5e-2),
+        # (1.0, torch.bfloat16, 5e-3, 5e-2),
+        (1.0, torch.float32, 1e-5, 5e-4),
+        # (1.0, torch.bfloat16, 5e-0, 5e1),
+        (1.0, torch.float32, 1e-3, 5e-2),
     ],
 )
-@pytest.mark.parametrize("bias", [True, False])
-@pytest.mark.parametrize("label_smoothing", [0, 0.1])
-def test_correctness(
-    B, T, H, V, scalar, dtype, bias, label_smoothing, reduction, atol, rtol
-):
+@pytest.mark.parametrize("temperature, beta", [(1.0, 0.5), (2.0, 0.1)])
+def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
     device = "cuda"
-    torch_lm_head_ce = TorchLMHeadCE(
+    torch_lm_head_ce = TorchLMHeadJSD(
         H=H,
         V=V,
-        bias=bias,
-        label_smoothing=label_smoothing,
-        reduction=reduction,
         dtype=dtype,
+        temperature=temperature,
+        beta=beta,
     ).to(device)
-    liger_lm_head_ce = LigerLMHeadCE(
+    liger_lm_head_ce = LigerLMHeadJSD(
         H=H,
         V=V,
-        bias=bias,
-        label_smoothing=label_smoothing,
-        reduction=reduction,
         dtype=dtype,
     ).to(device)
 
     # init the linear in all CEs with the same weights
-    torch_lm_head_ce.lin.weight.data = liger_lm_head_ce.lin.weight.data = torch.rand(
-        V, H, device=device, dtype=dtype
-    )
-
-    if bias:
-        torch_lm_head_ce.lin.bias.data = liger_lm_head_ce.lin.bias.data = torch.rand(
-            V, device=device, dtype=dtype
-        )
+    torch_lm_head_ce.student_lin.weight.data = (
+        liger_lm_head_ce.student_lin.weight.data
+    ) = torch.rand(V, H, device=device, dtype=dtype)
+    torch_lm_head_ce.teacher_lin.weight.data = (
+        liger_lm_head_ce.teacher_lin.weight.data
+    ) = torch.rand(V, H, device=device, dtype=dtype)
 
     _tensor = torch.randn(B * T, H, device=device, dtype=dtype) * scalar
     _input1 = _tensor.detach().clone().requires_grad_(True)
     _input2 = _tensor.detach().clone().requires_grad_(True)
 
-    target = torch.randint(0, V, (B * T,), device=device, dtype=torch.long)
+    teacher_input = torch.randn(B * T, H, device=device, dtype=dtype) * scalar
 
-    output1 = torch_lm_head_ce(_input1, target)
-    output2 = liger_lm_head_ce(_input2, target)
+    output1 = torch_lm_head_ce(_input1, teacher_input)
+    output2 = liger_lm_head_ce(_input2, teacher_input)
 
     assert_verbose_allclose(output1, output2, atol=atol, rtol=rtol)
 
@@ -132,16 +130,8 @@ def test_correctness(
     assert_verbose_allclose(_input1.grad, _input2.grad, atol=atol, rtol=rtol)
 
     assert_verbose_allclose(
-        torch_lm_head_ce.lin.weight.grad,
-        liger_lm_head_ce.lin.weight.grad,
+        torch_lm_head_ce.student_lin.weight.grad,
+        liger_lm_head_ce.student_lin.weight.grad,
         atol=atol,
         rtol=rtol,
     )
-
-    if bias:
-        assert_verbose_allclose(
-            torch_lm_head_ce.lin.bias.grad,
-            liger_lm_head_ce.lin.bias.grad,
-            atol=atol,
-            rtol=rtol,
-        )