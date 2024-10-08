Fix test to avoid exploding gradients

src/liger_kernel/ops/fused_linear_jsd.py
@@ -46,7 +46,6 @@ def fused_linear_jsd_forward(
     # we use fp32 for loss accumulator
     loss_1d = torch.zeros((BT, V), dtype=torch.float32, device=device)
 
-    print(num_chunks)
     for chunk_id in range(num_chunks):
         start_idx = chunk_id * chunk_size
         end_idx = min((chunk_id + 1) * chunk_size, BT)
@@ -58,8 +57,6 @@ def fused_linear_jsd_forward(
         # when doing matmul, use the original precision, shape: chunk_size x V
         student_logits_chunk = student_input_chunk @ student_weight.t()
         teacher_logits_chunk = teacher_input_chunk @ teacher_weight.t()
-        print(f"{student_weight.shape=}")
-        print(f"{student_input_chunk.shape=}, {student_logits_chunk.shape=}")
         chunk_n_rows = student_logits_chunk.shape[0]
 
         # unreduced loss
@@ -110,7 +107,7 @@ def fused_linear_jsd_forward(
             )
 
     loss = torch.sum(loss_1d) / BT
-    return loss, grad_input, grad_weight
+    return loss.to(student_input.dtype), grad_input, grad_weight
 
 
 def fused_linear_jsd_backward(grad_output, grad_input, grad_weight):

test/transformers/test_fused_linear_jsd.py
@@ -23,15 +23,16 @@ class TorchLMHeadJSD(torch.nn.Module):
         H: int,
         V: int,
         dtype: torch.dtype,
+        device: torch.device,
         temperature: float = 1.0,
         beta: float = 0.5,
     ):
         super().__init__()
         self.student_lin = torch.nn.Linear(
-            in_features=H, out_features=V, bias=False, dtype=dtype
+            in_features=H, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.teacher_lin = torch.nn.Linear(
-            in_features=H, out_features=V, bias=False, dtype=dtype
+            in_features=H, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.jsd = TorchJSD(beta, dtype=dtype)
         self.temperature = temperature
@@ -51,15 +52,16 @@ class LigerLMHeadJSD(torch.nn.Module):
         H: int,
         V: int,
         dtype: torch.dtype,
+        device: torch.device,
         temperature: float = 1.0,
         beta: float = 0.5,
     ):
         super().__init__()
         self.student_lin = torch.nn.Linear(
-            in_features=H, out_features=V, bias=False, dtype=dtype
+            in_features=H, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.teacher_lin = torch.nn.Linear(
-            in_features=H, out_features=V, bias=False, dtype=dtype
+            in_features=H, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.fused_jsd = LigerFusedLinearJSD(beta, temperature)
 
@@ -81,10 +83,10 @@ class LigerLMHeadJSD(torch.nn.Module):
     "B, T, H, V",
     [
         (2, 4, 2048, 3200),
-        (8, 2048, 4096, 32000),  # llama2, mistral
+        (2, 2048, 4096, 32000),  # llama2, mistral
         # Comment out to speed up testing
-        (4, 2048, 4096, 128256),  # llama3 8B
-        (4, 1024, 8192, 128256),  # llama3 70B
+        # (4, 2048, 4096, 128256),  # llama3 8B
+        # (4, 1024, 8192, 128256),  # llama3 70B
         (4, 423, 8192, 32000),  # random shape
     ],
 )
@@ -93,8 +95,6 @@ class LigerLMHeadJSD(torch.nn.Module):
     [
         (1.0, torch.bfloat16, 5e-3, 5e-2),
         (1.0, torch.float32, 1e-5, 5e-4),
-        (1.0, torch.bfloat16, 5e-0, 5e1),
-        (1.0, torch.float32, 1e-3, 5e-2),
     ],
 )
 @pytest.mark.parametrize(
@@ -110,6 +110,7 @@ def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
         H=H,
         V=V,
         dtype=dtype,
+        device=device,
         temperature=temperature,
         beta=beta,
     ).to(device)
@@ -117,6 +118,7 @@ def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
         H=H,
         V=V,
         dtype=dtype,
+        device=device,
         temperature=temperature,
         beta=beta,
     ).to(device)
@@ -129,23 +131,23 @@ def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
         liger_lm_head_jsd.teacher_lin.weight.data
     ) = torch.rand(V, H, device=device, dtype=dtype)
 
-    _tensor = torch.randn(B * T, H, device=device, dtype=dtype) * scalar
+    _tensor = torch.rand(B * T, H, device=device, dtype=dtype) * scalar
     _input1 = _tensor.detach().clone().requires_grad_(True)
     _input2 = _tensor.detach().clone().requires_grad_(True)
 
-    teacher_input = torch.randn(B * T, H, device=device, dtype=dtype) * scalar
-
+    teacher_input = torch.rand(B * T, H, device=device, dtype=dtype) * scalar
+    
     output1 = torch_lm_head_jsd(_input1, teacher_input)
     output2 = liger_lm_head_jsd(_input2, teacher_input)
 
-    assert_verbose_allclose(output1, output2, atol=atol, rtol=rtol)
-
+    assert torch.allclose(output1, output2, atol=atol, rtol=rtol)
+    
     output1.backward()
     output2.backward()
 
-    assert_verbose_allclose(_input1.grad, _input2.grad, atol=atol, rtol=rtol)
+    assert torch.allclose(_input1.grad, _input2.grad, atol=atol, rtol=rtol)
 
-    assert_verbose_allclose(
+    assert torch.allclose(
         torch_lm_head_jsd.student_lin.weight.grad,
         liger_lm_head_jsd.student_lin.weight.grad,
         atol=atol,
@@ -157,20 +159,18 @@ def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
     "B, T, H, V",
     [
         (2, 4, 512, 512),  # The test does not work on some CI GPUs. Issue #160
-        # (8, 2048, 4096, 32000),  # llama2, mistral
+        (2, 2048, 4096, 3200),  # llama2, mistral
         # Comment out to speed up testing
         # (4, 2048, 4096, 128256),  # llama3 8B
         # (4, 1024, 8192, 128256),  # llama3 70B
-        # (4, 423, 8192, 32000),  # random shape
+        (4, 423, 8192, 32000),  # random shape
     ],
 )
 @pytest.mark.parametrize(
     "scalar, dtype, atol, rtol",
     [
-        # (1.0, torch.bfloat16, 5e-3, 5e-2),
+        (1.0, torch.bfloat16, 5e-3, 5e-2),
         (1.0, torch.float32, 1e-5, 5e-4),
-        # (1.0, torch.bfloat16, 5e-0, 5e1),
-        (1.0, torch.float32, 1e-3, 5e-2),
     ],
 )
 @pytest.mark.parametrize("temperature, beta", [(1.0, 0.5), (2.0, 0.1)])
@@ -185,10 +185,10 @@ def test_correctness_functional(
     _weight2 = _weight.detach().clone().requires_grad_(True)
     teacher_weight = torch.rand(V, H, device=device, dtype=dtype)
 
-    _tensor = torch.randn(B * T, H, device=device, dtype=dtype) * scalar
+    _tensor = torch.rand(B * T, H, device=device, dtype=dtype) * scalar
     _input1 = _tensor.detach().clone().requires_grad_(True)
     _input2 = _tensor.detach().clone().requires_grad_(True)
-    teacher_input = torch.randn(B * T, H, device=device, dtype=dtype) * scalar
+    teacher_input = torch.rand(B * T, H, device=device, dtype=dtype) * scalar
 
     output1 = liger_fused_linear_jsd(
         _input1, _weight1, teacher_input, teacher_weight, beta, temperature
@@ -197,16 +197,16 @@ def test_correctness_functional(
         _input2, _weight2, teacher_input, teacher_weight, beta, temperature
     )
 
-    assert_verbose_allclose(output1, output2, atol=atol, rtol=rtol)
+    assert torch.allclose(output1, output2, atol=atol, rtol=rtol)
 
     output1.backward()
     output2.backward()
 
-    assert_verbose_allclose(_input1.grad, _input2.grad, atol=atol, rtol=rtol)
+    assert torch.allclose(_input1.grad, _input2.grad, atol=atol, rtol=rtol)
 
-    assert_verbose_allclose(
+    assert torch.allclose(
         _weight1.grad,
         _weight2.grad,
         atol=atol,
-        rtol=rtol,
+        rtol=rtol
     )