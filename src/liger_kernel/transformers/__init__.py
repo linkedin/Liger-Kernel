9b10f48f17af28d7319869f2f8a0c18d93f60e21
Tyler Romero <tyler.alexander.romero@gmail.com>
2024-10-10 15:16:00 -0700

    Monkeypatch for Llama 3.2-Vision (#282)
    ## Summary
    Add monkeypatch to support [Llama
    3.2-Vision](https://github.com/huggingface/transformers/pull/33703/files#diff-d804e851851cdebeb8048938f1f8beec1cfa78bf7b1f06af86faa450f9d18def)
    models.
    
    ## Details
    Llama 3.2-Vision is a multimodal model. It is also only available in
    transformers>=4.45.0.
    
    Torchvision is required to run the multimodal tests for Llama 3.2-Vision
    (the image processor requires it).
    
    
    ## Testing Done
    
    - Hardware Type: RTX 4090
    - [x] run `make test` to ensure correctness
    - [x] run `make checkstyle` to ensure code style
    - [x] run `make test-convergence` to ensure convergence
    
    ---------
    
    Co-authored-by: Shao Tang <tangshao28@gmail.com>
    Co-authored-by: Steven Shimizu <shimizust@gmail.com>
    Co-authored-by: shivam15s <shivam15800@gmail.com>

--- src/liger_kernel/transformers/__init__.py
+++ src/liger_kernel/transformers/__init__.py
@@ -16,6 +16,7 @@ from liger_kernel.transformers.monkey_patch import (  # noqa: F401
     apply_liger_kernel_to_llama,
     apply_liger_kernel_to_mistral,
     apply_liger_kernel_to_mixtral,
+    apply_liger_kernel_to_mllama,
     apply_liger_kernel_to_phi3,
     apply_liger_kernel_to_qwen2,
     apply_liger_kernel_to_qwen2_vl,
