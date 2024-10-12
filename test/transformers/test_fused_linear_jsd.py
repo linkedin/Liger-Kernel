Fix torch jsd formula

test/transformers/test_fused_linear_jsd.py
@@ -40,7 +40,7 @@ class TorchLMHeadJSD(torch.nn.Module):
         self.jsd = TorchJSD(beta=beta, ignore_index=ignore_index, dtype=dtype)
         self.temperature = temperature
 
-    def forward(self, student_input, teacher_input, label):
+    def forward(self, student_input, teacher_input, label=None):
         student_logits = self.student_lin(student_input)
         teacher_logits = self.teacher_lin(teacher_input)
         student_prob = torch.log_softmax(student_logits / self.temperature, dim=-1)
@@ -68,10 +68,10 @@ class LigerLMHeadJSD(torch.nn.Module):
             in_features=H, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.fused_jsd = LigerFusedLinearJSD(
-            beta=beta, ignore_index=ignore_index, temperature=temperature
+            jsd_beta=beta, ignore_index=ignore_index, temperature=temperature
         )
 
-    def forward(self, student_input, teacher_input, label):
+    def forward(self, student_input, teacher_input, label=None):
         return self.fused_jsd(
             student_input,
             self.student_lin.weight,
@@ -144,10 +144,11 @@ def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
 
     teacher_input = torch.rand(B * T, H, device=device, dtype=dtype) * scalar
 
-    output1 = torch_lm_head_jsd(_input1, teacher_input)
-    output2 = liger_lm_head_jsd(_input2, teacher_input)
+    with torch.autograd.detect_anomaly():
+        output1 = torch_lm_head_jsd(_input1, teacher_input)
+        output2 = liger_lm_head_jsd(_input2, teacher_input)
 
-    assert torch.allclose(output1, output2, atol=atol, rtol=rtol)
+        assert torch.allclose(output1, output2, atol=atol, rtol=rtol)
 
     output1.backward()
     output2.backward()

test/transformers/test_jsd.py
@@ -45,7 +45,7 @@ class JSD(torch.nn.Module):
             else:
                 loss = loss.sum() / n_non_ignore
         else:
-            loss = loss.sum() / log_q.shape()
+            loss = loss.sum() / log_q.shape[0]
         return loss.to(self.dtype)
 
 
@@ -282,11 +282,15 @@ def test_correctness_with_ignore_index(B, T, V, ignore_index, dtype, atol, rtol)
 @pytest.mark.parametrize(*_SHAPE_PARAMS)
 @pytest.mark.parametrize(*_DTYPE_PARAMS)
 @pytest.mark.parametrize(
-    "beta, is_last_layer",
+    "beta, ignore_index, is_last_layer",
     [
-        (0.5, False),
-        (0.1, True),
+        (0.5, 2, False),
+        (0.1, 42, True),
     ],
 )
-def test_correctness_functional(B, T, V, beta, is_last_layer, dtype, atol, rtol):
-    _test_correctness_functional(B, T, V, beta, is_last_layer, dtype, atol, rtol)
+def test_correctness_functional(
+    B, T, V, beta, ignore_index, is_last_layer, dtype, atol, rtol
+):
+    _test_correctness_functional(
+        B, T, V, beta, ignore_index, is_last_layer, dtype, atol, rtol
+    )