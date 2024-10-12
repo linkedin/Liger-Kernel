d2d6e44cbebbe590b09b139eb3c9410037a1e6a5
Tcc0403 <tcc32789@gmail.com>
2024-10-12 14:00:49 +0800

    Fix merge conflicts

--- src/liger_kernel/ops/cross_entropy.py
+++ src/liger_kernel/ops/cross_entropy.py
@@ -2,9 +2,10 @@ import torch
 import triton
 import triton.language as tl
 
+from liger_kernel.ops.utils import element_mul_kernel
+
 _TRUE = tl.constexpr(1)
 _FALSE = tl.constexpr(0)
-from liger_kernel.ops.utils import element_mul_kernel
 
 
 @triton.jit
@@ -186,42 +187,6 @@ def liger_cross_entropy_kernel(
 MAX_FUSED_SIZE = 65536 // 2  # the best size we found by manually tuning
 
 
-@triton.jit
-def element_mul_kernel(
-    X_ptr,
-    X_stride,
-    grad_output_ptr,
-    n_cols,
-    BLOCK_SIZE: tl.constexpr,
-):
-    """
-    This function multiplies each element of the tensor pointed by X_ptr with the value pointed by grad_output_ptr.
-    The multiplication is performed in-place on the tensor pointed by X_ptr.
-
-    Parameters:
-    X_ptr: Pointer to the input tensor.
-    X_stride (int): The stride of the input tensor.
-    grad_output_ptr: Pointer to the gradient output value.
-    n_cols (int): The number of columns in the input tensor.
-    BLOCK_SIZE (int): The block size for Triton operations.
-    """
-
-    # Get the program ID and convert it to int64 to avoid overflow
-    program_id = tl.program_id(0).to(tl.int64)
-
-    # Locate the start index
-    X_ptr += program_id * X_stride
-
-    # Load the gradient output value
-    grad_output = tl.load(grad_output_ptr)
-
-    # Perform the element-wise multiplication
-    for i in range(0, n_cols, BLOCK_SIZE):
-        X_offsets = i + tl.arange(0, BLOCK_SIZE)
-        X_block = tl.load(X_ptr + X_offsets, mask=X_offsets < n_cols)
-        tl.store(X_ptr + X_offsets, X_block * grad_output, mask=X_offsets < n_cols)
-
-
 _bool_to_return_z_loss = {
     True: _TRUE.value,
     False: _FALSE.value,
@@ -247,7 +212,6 @@ def cross_entropy_forward(
             return_z_loss in _bool_to_return_z_loss
         ), f"return_z_loss must be True or False. Got: {return_z_loss}"
 
-def cross_entropy_forward(_input, target, ignore_index, label_smoothing, reduction):
     BT, V = _input.shape
     n_rows = BT
 
