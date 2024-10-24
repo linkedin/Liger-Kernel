ae529cc280f7b46b16314ac68097d2245e5b59af
Parag Ekbote <thecoolekbote189@gmail.com>
2024-10-24 06:45:00 +0000

    Added Extensions.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.duration',]
 
 templates_path = ['_templates']
 exclude_patterns = []
