Fix FusedLinearJSD precision issue when using AMP (#336)
## Summary
<!--- This is a required section; please describe the main purpose of
this proposed code change. --->
1. make sure all the computation between logit to final JSD loss happen
on FP32
2. make sure FLJSD works properly under mixed precision scenario, also
add a test to guard
3. the Torch CE loss impl we use in testing FLCE misses out the fp32
cast for logits, add it back. **NOTE: we should definitely jus switch
directly to [HF
impl](https://github.com/huggingface/transformers/blob/main/src/transformers/loss/loss_utils.py#L32)
for testing to ensure always doing apple-to-apple comparison. See the
added TODO item.**


<!---
## Details
This is an optional section; is there anything specific that reviewers
should be aware of?
--->

## Testing Done
<!--- This is a required section; please describe how this change was
tested. --->

<!-- 
Replace BLANK with your device type. For example, A100-80G-PCIe

Complete the following tasks before sending your PR, and replace `[ ]`
with
`[x]` to indicate you have done them. 
-->

- Hardware Type: <BLANK>
- [x] run `make test` to ensure correctness
- [x] run `make checkstyle` to ensure code style
- [x] run `make test-convergence` to ensure convergence

src/liger_kernel/ops/fused_linear_jsd.py
@@ -4,7 +4,7 @@ import torch
 import triton
 
 from liger_kernel.ops.jsd import _jsd_kernel
-from liger_kernel.ops.utils import element_mul_kernel
+from liger_kernel.ops.utils import amp_custom_bwd, amp_custom_fwd, element_mul_kernel
 
 # The hard limit of TRITON_MAX_TENSOR_NUMEL is 1048576 https://github.com/triton-lang/triton/blob/ba42a5c68fd0505f8c42f4202d53be0f8d9a5fe0/python/triton/language/core.py#L19
 # However, setting limit as 65536 as in LayerNorm tutorial is faster because of less register spilling
@@ -24,6 +24,7 @@ def fused_linear_jsd_forward(
     temperature,
 ):
     device = student_input.device
+    dtype = student_input.dtype
 
     # inputs have shape: BT x H
     # materialized activations will have shape: BT x V
@@ -64,9 +65,15 @@ def fused_linear_jsd_forward(
         student_input_chunk = student_input[start_idx:end_idx]
         teacher_input_chunk = teacher_input[start_idx:end_idx]
 
-        # when doing matmul, use the original precision, shape: chunk_size x V
-        student_logits_chunk = student_input_chunk @ student_weight.t()
-        teacher_logits_chunk = teacher_input_chunk @ teacher_weight.t()
+        # shape: chunk_size x V
+        # For anything starting from logits to the final JSD loss, we do computation
+        # in FP32 to avoid losing numerical stability.
+        student_logits_chunk = (student_input_chunk @ student_weight.t()).to(
+            torch.float32
+        )
+        teacher_logits_chunk = (teacher_input_chunk @ teacher_weight.t()).to(
+            torch.float32
+        )
         chunk_n_rows = student_logits_chunk.shape[0]
 
         # unreduced loss
@@ -113,18 +120,16 @@ def fused_linear_jsd_forward(
                 student_prob_chunk.shape
             )
         ) / temperature
+        # now we traverse back to grad w.r.t. input to `lm_head` and grad
+        # w.r.t. `lm_head` which should be computed in original dtype
+        student_logits_chunk = student_logits_chunk.to(dtype)
         grad_input[start_idx:end_idx] = student_logits_chunk @ student_weight
 
         if grad_weight is not None:
-            torch.addmm(
-                input=grad_weight,
-                mat1=student_logits_chunk.t(),  # gradients of logits_chunk
-                mat2=student_input_chunk,
-                out=grad_weight,
-            )
+            grad_weight.add_(student_logits_chunk.t() @ student_input_chunk)
 
     loss = torch.sum(loss_1d)
-    return loss.to(student_input.dtype), grad_input, grad_weight
+    return loss, grad_input, grad_weight
 
 
 def fused_linear_jsd_backward(grad_output, grad_input, grad_weight):
@@ -172,6 +177,7 @@ class LigerFusedLinearJSDFunction(torch.autograd.Function):
     """
 
     @staticmethod
+    @amp_custom_fwd
     def forward(
         ctx,
         student_input: torch.Tensor,
@@ -225,6 +231,7 @@ class LigerFusedLinearJSDFunction(torch.autograd.Function):
         return loss
 
     @staticmethod
+    @amp_custom_bwd
     def backward(ctx, grad_output):
         (grad_input, grad_weight) = ctx.saved_tensors
         grad_input, grad_weight = fused_linear_jsd_backward(

test/transformers/test_fused_linear_cross_entropy.py
@@ -22,6 +22,10 @@ class TorchLMHeadCE(torch.nn.Module):
     :param V: vocab size
     :param ignore_index: index to ignore
     :param reduction: reduction method
+
+    # TODO: if we bump CI env's `transformers` version to >= 4.46, we should just directly
+    # call https://github.com/huggingface/transformers/blob/main/src/transformers/loss/loss_utils.py#L32
+    # to be consistent with Hugging Face model implementation.
     """
 
     def __init__(
@@ -45,7 +49,7 @@ class TorchLMHeadCE(torch.nn.Module):
         )
 
     def forward(self, x, y):
-        logits = self.lin(x)
+        logits = self.lin(x).to(torch.float32)
         return self.ce_loss(logits, y)
 
 