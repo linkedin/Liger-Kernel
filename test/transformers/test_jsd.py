6952cb408f32f2814f4923bec8086452f2af30a5
ByronHsu <byronhsu1230@gmail.com>
2024-11-04 21:57:19 +0000

    Aggressively trim test bloat

--- test/transformers/test_jsd.py
+++ test/transformers/test_jsd.py
@@ -53,20 +53,8 @@ _SHAPE_PARAMS = (
     "B, T, V",
     [
         (2, 1024, 3200),
-        (2, 1024, 3200),
         # weird shape
         (41, 401, 1271),
-        pytest.param(
-            1,
-            4096,
-            128256,
-            marks=pytest.mark.skipif(
-                torch.cuda.get_device_properties(0).total_memory
-                < 36 * 1000 * 1000 * 1000,
-                reason="This test requires a GPU with at least 36GB of memory",
-            ),
-        ),
-        (3, 423, 1600),
     ],
 )
 
