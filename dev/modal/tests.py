ef3f55dcd06b4fca95a5b75c9fe51ef1b7b7bfef
Byron Hsu <byronhsu1230@gmail.com>
2024-11-04 17:04:47 -0800

    merge two tests into one (#349)
    ## Summary
    <!--- This is a required section; please describe the main purpose of
    this proposed code change. --->
    
    <!---
    ## Details
    This is an optional section; is there anything specific that reviewers
    should be aware of?
    --->
    
    remove the launching overhead of the 2nd container
    
    ## Testing Done
    <!--- This is a required section; please describe how this change was
    tested. --->
    
    <!-- 
    Replace BLANK with your device type. For example, A100-80G-PCIe
    
    Complete the following tasks before sending your PR, and replace `[ ]`
    with
    `[x]` to indicate you have done them. 
    -->
    
    - Hardware Type: <BLANK>
    - [ ] run `make test` to ensure correctness
    - [ ] run `make checkstyle` to ensure code style
    - [ ] run `make test-convergence` to ensure convergence

--- dev/modal/tests.py
+++ dev/modal/tests.py
@@ -8,15 +8,15 @@ image = modal.Image.debian_slim().pip_install_from_pyproject(
     ROOT_PATH / "pyproject.toml", optional_dependencies=["dev"]
 )
 
-app = modal.App("liger_convergence_test", image=image)
+app = modal.App("liger_tests", image=image)
 
 # mount: add local files to the remote container
 repo = modal.Mount.from_local_dir(ROOT_PATH, remote_path="/root/liger-kernel")
 
 
-@app.function(gpu="A10G", mounts=[repo], timeout=60 * 20)
-def liger_convergence_test():
+@app.function(gpu="A10G", mounts=[repo], timeout=60 * 10)
+def liger_tests():
     import subprocess
-
     subprocess.run(["pip", "install", "-e", "."], check=True, cwd="/root/liger-kernel")
+    subprocess.run(["make", "test"], check=True, cwd="/root/liger-kernel")
     subprocess.run(["make", "test-convergence"], check=True, cwd="/root/liger-kernel")
