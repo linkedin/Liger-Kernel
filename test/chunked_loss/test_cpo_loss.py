6e0554794942fced55afaf551222d7e71f122aff
pramodith <16939722+pramodith@users.noreply.github.com>
2024-11-14 17:02:24 +0000

    Initial commit

--- test/chunked_loss/test_cpo_loss.py
+++ test/chunked_loss/test_cpo_loss.py
@@ -5,34 +5,35 @@ import pytest
 import torch
 import torch.nn.functional as F
 
-from liger_kernel.chunked_loss.orpo_loss import LigerFusedLinearORPOFunction
+from liger_kernel.chunked_loss.cpo_loss import LigerFusedLinearCPOFunction
 
 # set random seed globally
 set_seed()
 
 
-class HFORPOLoss(HFAlignmentLoss):
+class HFCPOLoss(HFAlignmentLoss):
     """
-    Implementation of the Odds Ratio Preference Optimization (ORPO) loss,
-    adapted from Hugging Face's implementation.
-    Reference: https://github.com/huggingface/trl/blob/main/trl/trainer/orpo_trainer.py
+    HF's implementation of CPO loss in TRL. https://github.com/huggingface/trl/blob/main/trl/trainer/cpo_trainer.py
     """
 
-    def __init__(self, ignore_index: int = -100, beta: float = 0.1):
-        super().__init__(beta=beta, ignore_index=ignore_index)
+    def __init__(
+        self,
+        alpha: float = 1.0,
+        beta: float = 0.1,
+        ignore_index: int = -100,
+        label_smoothing: float = 0.0,
+    ):
+        super().__init__(alpha=alpha, beta=beta, ignore_index=ignore_index)
+        # Sigmoid defaults to the CPO loss defined in the paper listed above.
+        self.loss_type = "sigmoid"
+        self.label_smoothing = label_smoothing
 
     def alignment_loss(
         self,
         policy_chosen_logps: torch.FloatTensor,
         policy_rejected_logps: torch.FloatTensor,
-    ) -> Tuple[
-        torch.FloatTensor,
-        torch.FloatTensor,
-        torch.FloatTensor,
-        torch.FloatTensor,
-        torch.FloatTensor,
-    ]:
-        """Compute ORPO's odds ratio (OR) loss for a batch of policy and reference model log probabilities.
+    ) -> Tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]:
+        """Compute the CPO loss for a batch of policy and reference model log probabilities.
 
         Args:
             policy_chosen_logps: Log probabilities of the policy model for the chosen responses. Shape: (batch_size,)
@@ -40,19 +41,24 @@ class HFORPOLoss(HFAlignmentLoss):
 
         Returns:
             A tuple of three tensors: (losses, chosen_rewards, rejected_rewards).
-            The losses tensor contains the ORPO loss for each example in the batch.
+            The losses tensor contains the CPO loss for each example in the batch.
             The chosen_rewards and rejected_rewards tensors contain the rewards for the chosen and rejected responses, respectively.
-            The log odds ratio of the chosen responses over the rejected responses ratio for logging purposes.
-            The `log(sigmoid(log_odds_chosen))` for logging purposes.
         """
+        logits = policy_chosen_logps - policy_rejected_logps
 
-        # Derived from Eqs. (4) and (7) from https://huggingface.co/papers/2403.07691 by using log identities and exp(log(P(y|x)) = P(y|x)
-        log_odds = (policy_chosen_logps - policy_rejected_logps) - (
-            torch.log1p(-torch.exp(policy_chosen_logps))
-            - torch.log1p(-torch.exp(policy_rejected_logps))
-        )
-        ratio = F.logsigmoid(log_odds)
-        losses = self.beta * ratio
+        # The beta is a temperature parameter for the CPO loss, typically something in the range of 0.1 to 0.5.
+        # We ignore the reference model as beta -> 0. The label_smoothing parameter encodes our uncertainty about the labels and
+        # calculates a conservative CPO loss.
+        if self.loss_type == "sigmoid":
+            # This reduces to Equation 3 from the CPO paper when label_smoothing -> 0.
+            losses = (
+                F.logsigmoid(self.beta * logits) * (1 - self.label_smoothing)
+                + F.logsigmoid(-self.beta * logits) * self.label_smoothing
+            )
+        else:
+            raise ValueError(
+                f"Unknown loss type: {self.loss_type}. Should be one of ['sigmoid']"
+            )
 
         return losses
 
@@ -60,6 +66,7 @@ class HFORPOLoss(HFAlignmentLoss):
 @pytest.mark.parametrize(
     "B, T, H, V",
     [
+        # (1, 2, 12, 128),
         (8, 128, 1024, 4096),
         (3, 47, 31, 123),  # random shape
     ],
@@ -67,14 +74,18 @@ class HFORPOLoss(HFAlignmentLoss):
 @pytest.mark.parametrize(
     "scalar, dtype, atol, rtol",
     [
-        (1.0, torch.bfloat16, 5e-2, 5e-1),
+        (1.0, torch.bfloat16, 5e-3, 5e-3),
         (1.0, torch.float32, 1e-5, 5e-4),
     ],
 )
 @pytest.mark.parametrize("bias", [True, False])
-@pytest.mark.parametrize("ignore_index, beta", [(-100, 0.1), (42, 0.2)])
-def test_correctness(B, T, H, V, scalar, dtype, atol, rtol, bias, ignore_index, beta):
-    B = 2 * B  # orpo loss requires B to be even
+@pytest.mark.parametrize(
+    "ignore_index, beta, alpha", [(-100, 0.1, 1.0), (42, 0.2, 0.85)]
+)
+def test_correctness(
+    B, T, H, V, scalar, dtype, atol, rtol, bias, ignore_index, beta, alpha
+):
+    B = 2 * B  # cpo loss requires B to be even
 
     _input = torch.randn(B, T, H, device="cuda", dtype=dtype) * scalar
     input1 = _input.detach().clone().requires_grad_(True)
@@ -103,17 +114,11 @@ def test_correctness(B, T, H, V, scalar, dtype, atol, rtol, bias, ignore_index, 
     bias1 = _bias.detach().clone().requires_grad_(True) if bias else None
     bias2 = _bias.detach().clone().requires_grad_(True) if bias else None
 
-    loss1 = HFORPOLoss(ignore_index=ignore_index, beta=beta).get_batch_loss_metrics(
-        input1, weight1, target, bias1
+    loss1 = HFCPOLoss(ignore_index=ignore_index, beta=beta).get_batch_loss_metrics(
+        input1, weight1, target, bias1, alpha=alpha
     )
-    loss2 = LigerFusedLinearORPOFunction.apply(
-        input2,
-        weight2,
-        target,
-        bias2,
-        ignore_index,
-        beta,
-        True,
+    loss2 = LigerFusedLinearCPOFunction.apply(
+        input2, weight2, target, bias2, ignore_index, beta, alpha, True
     )
 
     assert_verbose_allclose(loss1, loss2, atol=atol, rtol=rtol)
