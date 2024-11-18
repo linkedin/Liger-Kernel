7a4457e3e229051a5b371f8ce2db863cffe330a8
Lin, Fanli <fanli.lin@intel.com>
2024-11-18 00:45:00 -0800

    fix typo

--- test/transformers/test_rms_norm.py
+++ test/transformers/test_rms_norm.py
@@ -103,7 +103,7 @@ class GemmaRMSNorm(nn.Module):
     [
         (LlamaRMSNorm, 0.0, "llama"),
         (GemmaRMSNorm, 1.0, "gemma"),
-        # (BaseRMSNorm, 0.0, "none"),
+        (BaseRMSNorm, 0.0, "none"),
     ],
 )
 @pytest.mark.parametrize(
