Fix numerical stability
Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

WIP

Signed-off-by: Austin Liu <austin362667@gmail.com>

src/liger_kernel/chunked_loss/fused_linear_distillation.py
@@ -60,14 +60,14 @@ class LigerFusedLinearDistillationBase(torch.autograd.Function):
             teacher_logits_chunk = teacher_logits_chunk + teacher_bias
         teacher_log_probs_chunk = F.log_softmax(teacher_logits_chunk.float(), dim=-1)
 
-        ce_loss = 0.0
-        if compute_ce_loss:
-            ce_loss = F.cross_entropy(
-                teacher_log_probs_chunk.view(-1, teacher_log_probs_chunk.shape[-1]),
-                target_chunk.view(-1),
-                reduction="sum",
-                ignore_index=ignore_index,
-            )
+        # ce_loss = 0.0
+        # if compute_ce_loss:
+        #     ce_loss = F.cross_entropy(
+        #         teacher_log_probs_chunk.view(-1, teacher_log_probs_chunk.shape[-1]),
+        #         target_chunk.view(-1),
+        #         reduction="sum",
+        #         ignore_index=ignore_index,
+        #     )
 
         # loss_mask = target_chunk != ignore_index
         # label_chunk = torch.where(loss_mask, target_chunk, 0)
@@ -96,9 +96,6 @@ class LigerFusedLinearDistillationBase(torch.autograd.Function):
         beta=0.5,
         compute_ce_loss=True,
         compiled=True,
-        use_ref_model=False,
-        ref_weight=None,
-        ref_bias=None,
         **loss_kwargs,
     ):
         """
@@ -262,15 +259,13 @@ class LigerFusedLinearDistillationBase(torch.autograd.Function):
                 compute_ce_loss=compute_ce_loss,
             )
         )
-        ce_loss = (
-            ce_loss
-            / (full_target != ignore_index).sum()
-        )
+        ce_loss = ce_loss / (full_target != ignore_index).sum()
+        
 
         distillation_loss = distillation_loss_fn(
             student_logps, teacher_logps
         )
         distillation_loss = distillation_loss / (full_target.shape[0])
 
-        loss = beta * ce_loss - (1-beta) * distillation_loss
+        loss = beta * ce_loss + (1-beta) * distillation_loss
         return loss, (distillation_loss, ce_loss, student_logps, teacher_logps)

src/liger_kernel/chunked_loss/jsd_loss.py
@@ -6,7 +6,7 @@ from liger_kernel.chunked_loss.fused_linear_distillation import LigerFusedLinear
 
 class LigerFusedLinearJSDFunction(LigerFusedLinearDistillationBase):
     @staticmethod
-    def distill_loss_fn(student_logps, teacher_logps, temperature=1.0):
+    def distillation_loss_fn(student_logps, teacher_logps):
         """
         Compute Jensen-Shannon Divergence loss between student and teacher distributions.
         Args:
@@ -25,18 +25,17 @@ class LigerFusedLinearJSDFunction(LigerFusedLinearDistillationBase):
         # teacher_probs = F.softmax(teacher_logits, dim=-1)
 
         # Compute mean distribution
-        mean_probs = (student_logps + teacher_logps) / 2
-
+        mean_probs = (torch.exp(student_logps) + torch.exp(teacher_logps)) / 2
         # Compute KL divergences
         student_kl = F.kl_div(
-            student_logps.log(),
+            student_logps,
             mean_probs,
             reduction='batchmean',
             log_target=False
         )
 
         teacher_kl = F.kl_div(
-            teacher_logps.log(),
+            teacher_logps,
             mean_probs,
             reduction='batchmean',
             log_target=False
@@ -56,7 +55,7 @@ class LigerFusedLinearJSDFunction(LigerFusedLinearDistillationBase):
         shift_labels: torch.LongTensor,
         beta: float = 0.5,
         ignore_index: int = -100,
-        temperature: float = 1.0,
+        # temperature: float = 1.0,
         compiled: bool = True,
     ):
         """
@@ -81,10 +80,10 @@ class LigerFusedLinearJSDFunction(LigerFusedLinearDistillationBase):
             teacher_input=teacher_input,
             teacher_weight=teacher_weight,
             target=shift_labels,
-            loss_fn=LigerFusedLinearJSDFunction.distill_loss_fn,
+            loss_fn=LigerFusedLinearJSDFunction.distillation_loss_fn,
             beta=beta,
             ignore_index=ignore_index,
-            temperature=temperature,
+            # temperature=temperature,
             compiled=compiled,
         )
 
@@ -103,7 +102,7 @@ class LigerFusedLinearJSDLoss(torch.nn.Module):
         self, 
         beta: float = 0.5, 
         ignore_index: int = -100, 
-        temperature: float = 1.0, 
+        # temperature: float = 1.0, 
         compiled: bool = False
     ):
         """
@@ -114,10 +113,10 @@ class LigerFusedLinearJSDLoss(torch.nn.Module):
             compiled (bool): Whether to use torch compile
         """
         super().__init__()
-        assert temperature != 0, "Temperature cannot be 0."
+        # assert temperature != 0, "Temperature cannot be 0."
         self.beta = beta
         self.ignore_index = ignore_index
-        self.temperature = temperature
+        # self.temperature = temperature
         self.compiled = compiled
 
     def forward(
@@ -149,6 +148,6 @@ class LigerFusedLinearJSDLoss(torch.nn.Module):
             shift_labels,
             self.beta,
             self.ignore_index,
-            self.temperature,
+            # self.temperature,
             self.compiled
         )

test/chunked_loss/test_jsd_loss.py
@@ -33,8 +33,8 @@ class NaiveJSDLoss(NaiveDistillationLoss):
 
     def distillation_loss(
         self,
-        student_logits: torch.FloatTensor,
-        teacher_logits: torch.FloatTensor,
+        student_logps: torch.FloatTensor,
+        teacher_logps: torch.FloatTensor,
     ) -> torch.FloatTensor:
         """Compute the JSD distillation loss for a batch of student and teacher logits.
 
@@ -45,23 +45,23 @@ class NaiveJSDLoss(NaiveDistillationLoss):
         Returns:
             The JSD distillation loss for the batch as a scalar tensor.
         """
-        print('hf', student_logits, teacher_logits, self.temperature)
+        print('hf', student_logps, teacher_logps, self.temperature)
 
-        student_logits = student_logits / self.temperature
-        teacher_logits = teacher_logits / self.temperature
-        student_probs = F.softmax(student_logits, dim=-1)
-        teacher_probs = F.softmax(teacher_logits, dim=-1)
+        # student_logits = student_logits / self.temperature
+        # teacher_logits = teacher_logits / self.temperature
+        student_probs = torch.exp(student_logps)
+        teacher_probs = torch.exp(teacher_logps)
 
         mean_probs = (student_probs + teacher_probs) / 2
 
         student_kl = F.kl_div(
-            F.log_softmax(student_logits, dim=-1),
+            student_logps,
             mean_probs,
             reduction='batchmean',
             log_target=False,
         )
         teacher_kl = F.kl_div(
-            F.log_softmax(teacher_logits, dim=-1),
+            teacher_logps,
             mean_probs,
             reduction='batchmean',
             log_target=False,
@@ -92,7 +92,7 @@ class TorchLMHeadJSD(torch.nn.Module):
     ):
         super().__init__()
         self.student_lin = torch.nn.Linear(
-            in_features=H, out_features=V, bias=False, dtype=dtype, device=device
+            in_features=H // 2, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.teacher_lin = torch.nn.Linear(
             in_features=H, out_features=V, bias=False, dtype=dtype, device=device
@@ -125,13 +125,13 @@ class LigerLMHeadJSD(torch.nn.Module):
     ):
         super().__init__()
         self.student_lin = torch.nn.Linear(
-            in_features=H, out_features=V, bias=False, dtype=dtype, device=device
+            in_features=H // 2, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.teacher_lin = torch.nn.Linear(
             in_features=H, out_features=V, bias=False, dtype=dtype, device=device
         )
         self.chunked_jsd = LigerFusedLinearJSDLoss(
-            beta=beta, ignore_index=ignore_index, temperature=temperature
+            beta=beta, ignore_index=ignore_index
         )
 
     def forward(self, student_input, teacher_input, target):
@@ -167,9 +167,9 @@ class LigerLMHeadJSD(torch.nn.Module):
     "temperature, beta",
     [
         (1.0, 0.5),
-        # (2.0, 0.1),
-        # (1.0, 0.0),  # FKL
-        # (1.0, 1.0),  # RKL
+        (1.0, 0.1),
+        (1.0, 0.0),  # FKL
+        (1.0, 1.0),  # RKL
     ],
 )
 def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
@@ -193,12 +193,12 @@ def test_correctness(B, T, H, V, scalar, dtype, beta, temperature, atol, rtol):
     # init the linear in all FusedLinearJSDs with the same weights
     torch_lm_head_jsd.student_lin.weight.data = (
         liger_lm_head_jsd.student_lin.weight.data
-    ) = torch.rand(V, H, device=device, dtype=dtype)
+    ) = torch.rand(V, H // 2, device=device, dtype=dtype)
     torch_lm_head_jsd.teacher_lin.weight.data = (
         liger_lm_head_jsd.teacher_lin.weight.data
     ) = torch.rand(V, H, device=device, dtype=dtype)
 
-    _tensor = torch.rand(B * T, H, device=device, dtype=dtype) * scalar
+    _tensor = torch.rand(B * T, H // 2, device=device, dtype=dtype) * scalar
     _input1 = _tensor.detach().clone().requires_grad_(True)
     _input2 = _tensor.detach().clone().requires_grad_(True)
 

test/utils.py
@@ -522,7 +522,7 @@ class NaiveDistillationLoss:
         self.ignore_index = ignore_index
 
     @abstractmethod
-    def distillation_loss(self, student_logits, teacher_logits):
+    def distillation_loss(self, student_logps, teacher_logps):
         """Abstract method for computing distillation loss."""
         pass
 
@@ -573,12 +573,30 @@ class NaiveDistillationLoss:
         student_logits = student_outputs.view(student_batch_seq_len_size, -1).float()
         teacher_logits = teacher_outputs.view(teacher_batch_seq_len_size, -1).float()
 
-        student_ce_loss = F.cross_entropy(
+
+
+        def cross_entropy_loss(logits, labels):
+            # Flatten the tokens
+            loss_fct = nn.CrossEntropyLoss(ignore_index=self.ignore_index)
+            logits = logits.view(-1, logits.shape[-1])
+            labels = labels.view(-1)
+            # Enable model parallelism
+            labels = labels.to(logits.device)
+            loss = loss_fct(logits, labels)
+            return loss
+
+        student_ce_loss = cross_entropy_loss(
             student_logits.view(-1, student_logits.shape[-1]),
             target.view(-1),
-            reduction='mean',
-            ignore_index=self.ignore_index,
         )
+
+
+        # student_ce_loss = F.cross_entropy(
+        #     student_logits.view(-1, student_logits.shape[-1]),
+        #     target.view(-1),
+        #     reduction='mean',
+        #     ignore_index=self.ignore_index,
+        # )
 
         student_logps = self.get_batch_logps(
             student_logits, target, average_log_prob=average_log_prob
@@ -618,7 +636,7 @@ class NaiveDistillationLoss:
             student_ce_loss,
         ) = forward_output
 
-        distill_loss = self.distillation_loss(student_logits, teacher_logits)
+        distill_loss = self.distillation_loss(student_logps, teacher_logps)
         loss = student_ce_loss * (self.beta) + distill_loss.mean() * (1 - self.beta)
         print('hf_base', student_ce_loss, distill_loss.mean(), self.beta, loss)
         return loss