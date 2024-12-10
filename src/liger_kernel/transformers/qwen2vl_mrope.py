78e8a85413f1db3a674bd717c42bba2a5b29fe88
Jiahao Li <liplus17@163.com>
2024-12-11 01:20:39 +0800

    Fix Qwen2VL mrope for transformers 4.47.0 (#464)
    ## Summary
    <!--- This is a required section; please describe the main purpose of
    this proposed code change. --->
    
    Fix https://github.com/linkedin/Liger-Kernel/issues/461
    
    <!---
    ## Details
    This is an optional section; is there anything specific that reviewers
    should be aware of?
    --->
    
    ## Testing Done
    <!--- This is a required section; please describe how this change was
    tested. --->
    
    <!-- 
    Replace BLANK with your device type. For example, A100-80G-PCIe
    
    Complete the following tasks before sending your PR, and replace `[ ]`
    with
    `[x]` to indicate you have done them. 
    -->
    
    - Hardware Type: A800-SXM4-80GB
    - [x] run `make test` to ensure correctness
    - [x] run `make checkstyle` to ensure code style
    - [x] run `make test-convergence` to ensure convergence

--- src/liger_kernel/transformers/qwen2vl_mrope.py
+++ src/liger_kernel/transformers/qwen2vl_mrope.py
@@ -8,8 +8,8 @@ def liger_multimodal_rotary_pos_emb(q, k, cos, sin, mrope_section, unsqueeze_dim
     Args:
         q (torch.Tensor): The query tensor of shape (bsz, n_q_head, seq_len, head_dim).
         k (torch.Tensor): The key tensor of shape (bsz, n_kv_head, seq_len, head_dim).
-        cos (torch.Tensor): The cosine tensor of shape (3, 1, seq_len, head_dim).
-        sin (torch.Tensor): The sine tensor of shape (3, 1, seq_len, head_dim).
+        cos (torch.Tensor): The cosine tensor of shape (3, bsz, seq_len, head_dim).
+        sin (torch.Tensor): The sine tensor of shape (3, bsz, seq_len, head_dim).
         mrope_section (List[int]): The multimodal rope section for channel dimension of temporal, height and width in rope calculation.
         unsqueeze_dim (int, optional): The dimension to unsqueeze. Defaults to 1.
 
