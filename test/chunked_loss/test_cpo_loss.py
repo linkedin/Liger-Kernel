ce748882ff16d839702b21ea54cb6599dd349d6c
Austin Liu <austin362667@gmail.com>
2024-12-20 10:14:21 +0800

    Fix codestyle
    Signed-off-by: Austin Liu <austin362667@gmail.com>

--- test/chunked_loss/test_cpo_loss.py
+++ test/chunked_loss/test_cpo_loss.py
@@ -59,15 +59,15 @@ class HFCPOLoss(HFAlignmentLoss):
         # calculates a conservative CPO loss.
         if self.loss_type == "sigmoid":
             # This reduces to Equation 3 from the CPO paper when label_smoothing -> 0.
-            losses = -(
-                F.logsigmoid(self.beta * logits) * (1 - self.label_smoothing)
-                + F.logsigmoid(-self.beta * logits) * self.label_smoothing
+            losses = (
+                -F.logsigmoid(self.beta * logits) * (1 - self.label_smoothing)
+                - F.logsigmoid(-self.beta * logits) * self.label_smoothing
             )
         elif self.loss_type == "simpo":
             logits = logits - (self.simpo_gamma / self.beta)
-            losses = -(
-                F.logsigmoid(self.beta * logits) * (1 - self.label_smoothing)
-                + F.logsigmoid(-self.beta * logits) * self.label_smoothing
+            losses = (
+                -F.logsigmoid(self.beta * logits) * (1 - self.label_smoothing)
+                - F.logsigmoid(-self.beta * logits) * self.label_smoothing
             )
         else:
             raise ValueError(
