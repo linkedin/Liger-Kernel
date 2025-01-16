80cfc089f68bba080e297a3f987fd7b73465bbc2
jp1924 <jsb10121249@gmail.com>
2025-01-16 02:37:41 +0000

    Refactor: simplify lce_forward method and improve loss calculation

--- src/liger_kernel/transformers/model/llava.py
+++ src/liger_kernel/transformers/model/llava.py
@@ -4,8 +4,8 @@ from typing import Tuple
 from typing import Union
 
 import torch
+import torch.nn as nn
 
-from torch.nn import CrossEntropyLoss
 from transformers.models.llava.modeling_llava import _CONFIG_FOR_DOC
 from transformers.models.llava.modeling_llava import LLAVA_INPUTS_DOCSTRING
 from transformers.models.llava.modeling_llava import LlavaCausalLMOutputWithPast
@@ -72,7 +72,6 @@ def lce_forward(
     >>> processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     "USER:  \nWhat's the content of the image? ASSISTANT: The image features a busy city street with a stop sign prominently displayed"
     ```"""
-
     output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
     output_hidden_states = (
         output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
@@ -87,6 +86,9 @@ def lce_forward(
         else self.config.vision_feature_select_strategy
     )
 
+    if num_logits_to_keep != 0:
+        logger.warning_once("llava liger-kernel does not support num_logits_to_keep. This argument is ignored.")
+
     if (input_ids is None) ^ (inputs_embeds is not None):
         raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
 
@@ -95,18 +97,9 @@ def lce_forward(
             "You cannot specify both pixel_values and inputs_embeds at the same time, and must specify either one"
         )
 
-    legacy_processing = False
     if inputs_embeds is None:
         inputs_embeds = self.get_input_embeddings()(input_ids)
 
-        # if the number of image tokens is more than image embeddings seq length, then prob we expanded it in processing
-        # not very reliable, but we don't expect one to actually pass 500+ images for one prompt
-        # In case we're in decoding stage, legacy behavior is checked by presence of pixel values even if use_cache=True
-        legacy_processing = (
-            (input_ids == self.config.image_token_index).sum(1).max() < self.config.image_seq_length
-        ) or (input_ids.shape[-1] == 1 and pixel_values is not None)
-
-    image_features = None
     if pixel_values is not None:
         image_features = self.get_image_features(
             pixel_values=pixel_values,
@@ -114,164 +107,31 @@ def lce_forward(
             vision_feature_select_strategy=vision_feature_select_strategy,
         )
 
-    if image_features is not None and legacy_processing:
-        logger.warning_once(
-            "Expanding inputs for image tokens in LLaVa should be done in processing. "
-            "Please add `patch_size` and `vision_feature_select_strategy` to the model's processing config or set directly "
-            "with `processor.patch_size = {{patch_size}}` and processor.vision_feature_select_strategy = {{vision_feature_select_strategy}}`. "
-            "Using processors without these attributes in the config is deprecated and will throw an error in v4.50."
-        )
-        # prefill stage vs decoding stage (legacy behavior copied)
-        if input_ids.shape[1] != 1:
-            inputs_embeds, attention_mask, labels, position_ids = self._merge_input_ids_with_image_features(
-                image_features, inputs_embeds, input_ids, attention_mask, labels
-            )
-            cache_position = torch.arange(attention_mask.shape[1], device=attention_mask.device)
-        else:
-            # Retrieve the first layer to inspect the logits and mask out the hidden states
-            # that are set to 0
-            first_layer_past_key_value = past_key_values[0][0][:, :, :, 0]
-
-            # Sum all dimensions of head_dim (-2) to avoid random errors such as: https://github.com/huggingface/transformers/pull/28032#issuecomment-1863691941
-            batch_index, non_attended_tokens = torch.where(first_layer_past_key_value.float().sum(-2) == 0)
-
-            # Get the target length
-            target_length = input_ids.shape[1]
-            past_length = first_layer_past_key_value.shape[-1]
-
-            extended_attention_mask = torch.ones(
-                (attention_mask.shape[0], past_length),
-                dtype=attention_mask.dtype,
-                device=attention_mask.device,
-            )
-
-            # Filter out only the tokens that can be un-attended, this can happen
-            # if one uses Llava + Fused modules where the cache on the
-            # first iteration is already big enough, or if one passes custom cache
-            valid_indices = non_attended_tokens < extended_attention_mask.size(-1)
-            new_batch_index = batch_index[valid_indices]
-            new_non_attended_tokens = non_attended_tokens[valid_indices]
-
-            # Zero-out the places where we don't need to attend
-            extended_attention_mask[new_batch_index, new_non_attended_tokens] = 0
-
-            attention_mask = torch.cat((extended_attention_mask, attention_mask[:, -target_length:]), dim=1)
-            position_ids = torch.sum(attention_mask, dim=1).unsqueeze(-1) - 1
-            cache_position = torch.arange(attention_mask.shape[1], device=attention_mask.device)[-target_length:]
-
-    # TODO: @raushan retain only the new behavior after v4.47
-    elif image_features is not None:
         n_image_tokens = (input_ids == self.config.image_token_index).sum().item()
         n_image_features = image_features.shape[0] * image_features.shape[1]
-
         if n_image_tokens != n_image_features:
             raise ValueError(
                 f"Image features and image tokens do not match: tokens: {n_image_tokens}, features {n_image_features}"
             )
-        special_image_mask = (
-            (input_ids == self.config.image_token_index).unsqueeze(-1).expand_as(inputs_embeds).to(inputs_embeds.device)
-        )
+        special_image_mask = (input_ids == self.config.image_token_index).unsqueeze(-1)
+        special_image_mask = special_image_mask.expand_as(inputs_embeds).to(inputs_embeds.device)
         image_features = image_features.to(inputs_embeds.device, inputs_embeds.dtype)
         inputs_embeds = inputs_embeds.masked_scatter(special_image_mask, image_features)
 
-    # outputs = self.language_model(
-    #     labels=labels,
-    #     input_ids=input_ids,
-    #     attention_mask=attention_mask,
-    #     position_ids=position_ids,
-    #     past_key_values=past_key_values,
-    #     inputs_embeds=inputs_embeds,
-    #     use_cache=use_cache,
-    #     output_attentions=output_attentions,
-    #     output_hidden_states=True,
-    #     return_dict=return_dict,
-    #     cache_position=cache_position,
-    #     num_logits_to_keep=num_logits_to_keep,
-    # )
-
-    # if not return_dict:
-    #     output = (logits,) + outputs[1:]
-    #     return (loss,) + output if loss is not None else output
-
-    # return LlavaCausalLMOutputWithPast(
-    #     loss=outputs.loss,
-    #     logits=outputs.logits,
-    #     past_key_values=outputs.past_key_values,
-    #     hidden_states=outputs.hidden_states,
-    #     attentions=outputs.attentions,
-    #     image_hidden_states=image_features if pixel_values is not None else None,
-    # )
-
-    # outputs = self.language_model.model(
-    #     attention_mask=attention_mask,
-    #     position_ids=position_ids,
-    #     past_key_values=past_key_values,
-    #     inputs_embeds=inputs_embeds,
-    #     use_cache=use_cache,
-    #     output_attentions=output_attentions,
-    #     output_hidden_states=output_hidden_states,
-    #     return_dict=return_dict,
-    #     cache_position=cache_position,
-    #     num_logits_to_keep=num_logits_to_keep,
-    # )
-
-    # hidden_states = outputs[0]
-
-    # logits = None
-    # loss = None
-    # if self.training and (labels is not None):
-    #     shift_hidden_states = hidden_states[..., :-1, :].contiguous()
-    #     shift_labels = labels[..., 1:].contiguous()
-
-    #     # Flatten tokens
-    #     shift_hidden_states = shift_hidden_states.view(-1, self.config.text_config.hidden_size)
-    #     shift_labels = shift_labels.view(-1)
-
-    #     lce = LigerFusedLinearCrossEntropyLoss()
-    #     loss = lce(self.language_model.lm_head.weight, shift_hidden_states, shift_labels)
-    # else:
-    #     logits = self.language_model.lm_head(hidden_states)
-    #     if labels is not None:
-    #         # Upcast to float if we need to compute the loss to avoid potential precision issues
-    #         logits = logits.float()
-    #         # Shift so that tokens < n predict n
-    #         shift_logits = logits[..., :-1, :].contiguous()
-    #         shift_labels = labels[..., 1:].contiguous()
-    #         # Flatten the tokens
-    #         loss_fct = CrossEntropyLoss()
-    #         shift_logits = shift_logits.view(-1, self.config.text_config.vocab_size)
-    #         shift_labels = shift_labels.view(-1)
-    #         # Enable model parallelism
-    #         shift_labels = shift_labels.to(shift_logits.device)
-    #         loss = loss_fct(shift_logits, shift_labels)
-
-    # if not return_dict:
-    #     output = (logits,) + outputs[1:]
-    #     return (loss,) + output if loss is not None else output
-
-    # return LlavaCausalLMOutputWithPast(
-    #     loss=loss,
-    #     logits=logits,
-    #     past_key_values=outputs.past_key_values,
-    #     hidden_states=outputs.hidden_states,
-    #     attentions=outputs.attentions,
-    #     image_hidden_states=image_features if pixel_values is not None else None,
-    # )
-
-    outputs = self.language_model(
+    outputs = self.language_model.model(
         attention_mask=attention_mask,
         position_ids=position_ids,
         past_key_values=past_key_values,
         inputs_embeds=inputs_embeds,
         use_cache=use_cache,
         output_attentions=output_attentions,
-        output_hidden_states=True,
+        output_hidden_states=output_hidden_states,
         return_dict=return_dict,
         cache_position=cache_position,
-        num_logits_to_keep=num_logits_to_keep,
+        # num_logits_to_keep=num_logits_to_keep,
     )
 
-    hidden_states = outputs.hidden_states[-1]
+    hidden_states = outputs[0]
 
     logits = None
     loss = None
@@ -292,19 +152,19 @@ def lce_forward(
         loss = lce(self.language_model.lm_head.weight, shift_hidden_states, shift_labels)
     else:
         logits = self.language_model.lm_head(hidden_states)
-        if labels is not None:
-            # Upcast to float if we need to compute the loss to avoid potential precision issues
-            logits = logits.float()
-            # Shift so that tokens < n predict n
+        # Shift so that tokens < n predict n
+        if attention_mask is not None:
+            # we use the input attention mask to shift the logits and labels, because it is 2D.
+            # we also crop attn mask in case it is longer, which happens in PrefixTuning with peft
+            shift_attention_mask = attention_mask[:, -(logits.shape[1] - 1) :].to(logits.device)
+            shift_logits = logits[..., :-1, :][shift_attention_mask.to(logits.device) != 0].contiguous()
+            shift_labels = labels[..., 1:][shift_attention_mask.to(labels.device) != 0].contiguous()
+        else:
             shift_logits = logits[..., :-1, :].contiguous()
             shift_labels = labels[..., 1:].contiguous()
-            # Flatten the tokens
-            loss_fct = CrossEntropyLoss()
-            shift_logits = shift_logits.view(-1, self.config.text_config.vocab_size)
-            shift_labels = shift_labels.view(-1)
-            # Enable model parallelism
-            shift_labels = shift_labels.to(shift_logits.device)
-            loss = loss_fct(shift_logits, shift_labels)
+        # Flatten the tokens
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1).to(shift_logits.device))
 
     if not return_dict:
         output = (logits,) + outputs[1:]
