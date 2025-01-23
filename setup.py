0186035ded4e3239f83fbce7457fa828ffd40ae4
Lin, Fanli <fanli.lin@intel.com>
2025-01-22 21:08:50 -0800

    update triton

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_default_dependencies():
     elif platform == "xpu":
         return [
             "torch>=2.6.0",
-            "pytorch-triton-xpu>=3.2.0",
+            "triton>=2.3.1",
         ]
 
 
