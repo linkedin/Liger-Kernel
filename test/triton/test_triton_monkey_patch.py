61ebff3e8b99cf536c0f7dde824f5628ec6cf841
Fanli Lin <fanli0116@gmail.com>
2025-01-24 08:20:56 +0800

    [tests] use a valid hexadecimal string instead of a placeholder (#535)
    ## Summary
    In the latest code of `get_cache_manager` of the triton library
    (https://github.com/triton-lang/triton/blob/1bb8b8055c81f6bb85055645a20e0dbd27d5295f/python/triton/runtime/cache.py#L261),
    the key needs to be a valid hexadecimal string, otherwise the test will
    fail.
    
    This PR fixes the issue by using a valid hex string instead of a
    placeholder.
    
    ## Testing Done
    The test passes both on CUDA and XPU.
    ```bash
    ========================================================== PASSES ===========================================================
    ================================================== short test summary info ==================================================
    PASSED test/triton/test_triton_monkey_patch.py::test_import_custom_cache_manager
    =============================================== 1 passed, 1 warning in 0.12s ================================================
    ```
    
    On  Intel(R) Data Center GPU Max 1550 and NV GPU A100
    
    - Hardware Type: <BLANK>
    - [x] run `make test` to ensure correctness
    - [x] run `make checkstyle` to ensure code style
    - [ ] run `make test-convergence` to ensure convergence

--- test/triton/test_triton_monkey_patch.py
+++ test/triton/test_triton_monkey_patch.py
@@ -1,4 +1,10 @@
+import secrets
+
 import pytest
+
+
+def generate_random_hex(length=16):
+    return secrets.token_hex(length // 2)
 
 
 def test_import_from_root():
@@ -14,7 +20,8 @@ def test_import_custom_cache_manager():
     from liger_kernel.triton import apply_liger_triton_cache_manager
 
     apply_liger_triton_cache_manager()
-    cache_manager = get_cache_manager(key="test_hash")
+    random_hex_key = generate_random_hex(16)
+    cache_manager = get_cache_manager(key=random_hex_key)
     from liger_kernel.triton.monkey_patch import LigerTritonFileCacheManager
 
     assert isinstance(
