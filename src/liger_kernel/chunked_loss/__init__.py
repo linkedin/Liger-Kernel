377b874bf8e42eab8eae866fe85d28e1cff4a302
Kashif Rasul <kashif.rasul@gmail.com>
2025-01-31 13:17:44 +0100

    init

--- src/liger_kernel/chunked_loss/__init__.py
+++ src/liger_kernel/chunked_loss/__init__.py
@@ -4,3 +4,4 @@ from liger_kernel.chunked_loss.jsd_loss import LigerFusedLinearJSDLoss  # noqa: 
 from liger_kernel.chunked_loss.kto_loss import LigerFusedLinearKTOLoss  # noqa: F401
 from liger_kernel.chunked_loss.orpo_loss import LigerFusedLinearORPOLoss  # noqa: F401
 from liger_kernel.chunked_loss.simpo_loss import LigerFusedLinearSimPOLoss  # noqa: F401
+from liger_kernel.chunked_loss.grpo_loss import LigerFusedLinearGRPOLoss  # noqa: F401
