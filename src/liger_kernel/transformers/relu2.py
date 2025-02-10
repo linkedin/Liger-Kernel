dec3a3e201d5c4d8de76a608c56859e5f7920ec5
syx <yixinsong@sjtu.edu.cn>
2025-02-10 11:33:53 +0800

    add relu2 test

--- src/liger_kernel/transformers/relu2.py
+++ src/liger_kernel/transformers/relu2.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from liger_kernel.ops.relu2 import LigerReLU2MulFunction
 
 
-class LigerGEGLUMLP(nn.Module):
+class LigerRELU2MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
