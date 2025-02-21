7ecb24046f68712523e1c517f2f551e944f68ac6
shivam15s <shivam15800@gmail.com>
2025-02-21 07:41:32 +0000

    Revert "Improve Hugging Face SFT Script (#539)"
    This reverts commit 5476b20d3beb7d142805f11274aca167bbadeea3.

--- examples/huggingface/training_multimodal.py
+++ examples/huggingface/training_multimodal.py
@@ -1,11 +1,15 @@
 import os
+
+from dataclasses import dataclass
+
+import datasets
 import torch
 import transformers
-import datasets
-from dataclasses import dataclass
-from trl import SFTTrainer, SFTConfig
-from trl.trainer import ConstantLengthDataset
+
+from callback import EfficiencyCallback
 from datasets import Image as ImageFeature
+from trl import SFTTrainer
+
 from liger_kernel.transformers import monkey_patch
 
 
@@ -15,6 +19,8 @@ class CustomArguments:
     dataset: str = "HuggingFaceM4/the_cauldron"
     dataset_subset: str = "ai2d"
     dataset_split: str = "train"
+    max_seq_length: int = 512
+    dataset_text_field: str = "texts"
     use_liger: bool = False
 
 
@@ -89,34 +95,72 @@ def _format_for_convo(example, tokenizer):
 def train():
     parser = transformers.HfArgumentParser((transformers.TrainingArguments, CustomArguments))
     training_args, custom_args = parser.parse_args_into_dataclasses()
+    training_args.remove_unused_columns = False  # required to not drop the image column
+    training_args.dataset_kwargs = {"skip_prepare_dataset": True}
 
-    model, processor, image_token_id = construct_model_and_processor(
-        custom_args.model_name, custom_args.use_liger
+    model, processor, image_token_id = construct_model_and_processor(custom_args.model_name, custom_args.use_liger)
+
+    dataset = (
+        datasets.load_dataset(
+            custom_args.dataset,
+            custom_args.dataset_subset,
+            split=custom_args.dataset_split,
+        )
+        .map(
+            _validate_and_extract_the_cauldron,
+            batched=True,
+            num_proc=min(os.cpu_count(), 16),
+            desc="Extracting text and images",
+        )
+        .map(
+            _format_for_convo,
+            fn_kwargs={"tokenizer": processor.tokenizer},
+            desc="Formatting for convo",
+        )
+        .cast_column("images", ImageFeature())
+        .train_test_split(test_size=0.1)
     )
 
-    dataset = datasets.load_dataset(
-        custom_args.dataset, 
-        custom_args.dataset_subset, 
-        split=custom_args.dataset_split
-    )
+    train_dataset = dataset["train"]
+    eval_dataset = dataset["test"]
 
-    train_dataset, eval_dataset = prepare_dataset(dataset, processor, image_token_id)
+    def collate_fn(examples):
+        """
+        Taken directly from the TRL documentation with minor modifications:
+        https://huggingface.co/docs/trl/en/sft_trainer#a-custom-collator-for-processing-multi-modal-data
 
-    sft_config = SFTConfig(
-        output_dir=training_args.output_dir,
-        per_device_train_batch_size=training_args.per_device_train_batch_size,
-        per_device_eval_batch_size=training_args.per_device_eval_batch_size,
-        learning_rate=training_args.learning_rate,
-        num_train_epochs=training_args.num_train_epochs,
-        gradient_accumulation_steps=training_args.gradient_accumulation_steps,
-    )
+        Modifications:
+        1. `apply_chat_template` is used to preprocess the texts before training begins (see above)
+        2. `example["messages"]` -> `example["texts"]` to conform with the_cauldron dataset schema
+        3. Ignoring image tokens in the loss computation
+        """
+        # Get the texts and images
+        texts = [example["texts"] for example in examples]
+        images = [example["images"] for example in examples]
+
+        # Tokenize the texts and process the images
+        batch = processor(text=texts, images=images, return_tensors="pt", padding=True)
+
+        # The labels are the input_ids, and we mask the padding tokens in the loss computation
+        labels = batch["input_ids"].clone()
+        labels[labels == processor.tokenizer.pad_token_id] = -100
+
+        # Ignore the image token index in the loss computation
+        labels[labels == image_token_id] = -100
+        batch["labels"] = labels
+
+        return batch
 
     trainer = SFTTrainer(
         model=model,
-        args=sft_config,
+        args=training_args,
+        data_collator=collate_fn,
+        max_seq_length=custom_args.max_seq_length,
+        dataset_text_field=custom_args.dataset_text_field,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        processing_class=processor,
+        tokenizer=processor.tokenizer,
+        callbacks=[EfficiencyCallback()],
     )
     trainer.train()
 
