0089fd4f3ff5fb492d3b6c1017125d6485555c5a
shivam15s <shivam15800@gmail.com>
2025-04-11 19:21:46 +0000

    revert num warps change for non xpu

--- src/liger_kernel/ops/kl_div.py
+++ src/liger_kernel/ops/kl_div.py
@@ -117,7 +117,7 @@ def _kldiv_kernel_backward(
 def kldiv_forward_triton(y_pred, y_true, log_target, reduction, eps):  # [BT, V]
     BT, V = y_pred.shape
     BLOCK_SIZE = min(8192, triton.next_power_of_2(V)) if infer_device() == "xpu" else min(MAX_FUSED_SIZE, triton.next_power_of_2(V))
-    num_warps = 32 if infer_device() == "xpu" else triton.next_power_of_2(V)
+    num_warps = 32 if infer_device() == "xpu" else get_num_warps(BLOCK_SIZE)
 
     grid = (BT,)
     reduction = _str_to_reduction_mode[reduction]
@@ -156,7 +156,7 @@ def kldiv_forward_triton(y_pred, y_true, log_target, reduction, eps):  # [BT, V]
 def kldiv_backward_triton(target, grad_output, new_grads, log_target):
     BT, V = target.shape
     BLOCK_SIZE = min(8192, triton.next_power_of_2(V)) if infer_device() == "xpu" else min(MAX_FUSED_SIZE, triton.next_power_of_2(V))
-    num_warps = 32 if infer_device() == "xpu" else triton.next_power_of_2(V)
+    num_warps = 32 if infer_device() == "xpu" else get_num_warps(BLOCK_SIZE)
 
     grid = (BT,)
 
