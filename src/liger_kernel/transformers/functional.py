0534a93df01f5de56c76bd54ffaa30f504b00086
AndreSlavescu <andre.slavescu@gmail.com>
2025-04-28 00:54:58 -0400

    format

--- src/liger_kernel/transformers/functional.py
+++ src/liger_kernel/transformers/functional.py
@@ -15,6 +15,7 @@ from liger_kernel.ops.rope import LigerRopeFunction
 from liger_kernel.ops.sparsemax import LigerSparsemaxFunction
 from liger_kernel.ops.swiglu import LigerSiLUMulFunction
 from liger_kernel.ops.sparsemax import LigerSparsemaxFunction
+from liger_kernel.ops.swiglu import LigerSiLUMulFunction
 from liger_kernel.ops.tvd import LigerTVDLossFunction
 
 
@@ -160,14 +161,12 @@ def liger_kl_div(
         eps,
     )
 
+
 def liger_sparsemax(
     input,
     dim: int = -1,
 ):
-    return LigerSparsemaxFunction.apply(
-        input, 
-        dim
-    )
+    return LigerSparsemaxFunction.apply(input, dim)
 
 
 def liger_sparsemax(
