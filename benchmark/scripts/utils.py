9cd2ef99cb0313e4c1a7206a81642d7b577a639d
AndreSlavescu <andre.slavescu@gmail.com>
2025-04-30 16:27:43 -0400

    softmax + update mta to use custom softmax

--- benchmark/scripts/utils.py
+++ benchmark/scripts/utils.py
@@ -208,6 +208,13 @@ def update_benchmark_data_csv(
 
         # Need to convert benchmark_data into multiple rows based on x_values and y_values
         for x_value, y_value_50, y_value_20, y_value_80 in zip_longest(x_values, y_values_50, y_values_20, y_values_80):
+            if y_value_50 is None:
+                y_value_50 = float("nan")
+            if y_value_20 is None:
+                y_value_20 = float("nan")
+            if y_value_80 is None:
+                y_value_80 = float("nan")
+
             row = BenchmarkDataCSVRow(
                 x_value=x_value,
                 y_value_50=y_value_50,
