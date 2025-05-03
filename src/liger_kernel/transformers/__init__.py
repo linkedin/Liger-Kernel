7c7edeb396f22f73a6ebd90ee099cd2d164f67cf
Vaibhav Jindal <32337828+vaibhavjindal@users.noreply.github.com>
2025-05-02 19:17:33 -0700

    Qwen3 model support (#692)
    ## Summary
    <!--- This is a required section; please describe the main purpose of
    this proposed code change. --->
    This pull request introduces support for the Qwen3 model within the
    Liger-Kernel framework.
    Fixes: https://github.com/linkedin/Liger-Kernel/issues/690
    <!---
    ## Details
    This is an optional section; is there anything specific that reviewers
    should be aware of?
    --->
    
    ## Testing Done
    <!--- This is a required section; please describe how this change was
    tested. --->
    A simple check:
    ```
    from transformers import Qwen3ForCausalLM
    from liger_kernel.transformers.monkey_patch import apply_liger_kernel_to_qwen3
    
    apply_liger_kernel_to_qwen3()
    model = Qwen3ForCausalLM.from_pretrained("Qwen/Qwen3-8B")
    
    print(model)
    ```
    gives the following output:
    ![Screenshot 2025-05-02 at 12 15
    14 AM](https://github.com/user-attachments/assets/9d32456b-8f89-4a71-96f1-9f71ae2a2543)
    
    <!-- 
    Replace BLANK with your device type. For example, A100-80G-PCIe
    
    Complete the following tasks before sending your PR, and replace `[ ]`
    with
    `[x]` to indicate you have done them. 
    -->
    
    - Hardware Type: <BLANK>
    - [x] run `make test` to ensure correctness
    - [x] run `make checkstyle` to ensure code style
    - [x] run `make test-convergence` to ensure convergence

--- src/liger_kernel/transformers/__init__.py
+++ src/liger_kernel/transformers/__init__.py
@@ -39,6 +39,7 @@ if TYPE_CHECKING:
     from liger_kernel.transformers.monkey_patch import apply_liger_kernel_to_qwen2  # noqa: F401
     from liger_kernel.transformers.monkey_patch import apply_liger_kernel_to_qwen2_5_vl  # noqa: F401
     from liger_kernel.transformers.monkey_patch import apply_liger_kernel_to_qwen2_vl  # noqa: F401
+    from liger_kernel.transformers.monkey_patch import apply_liger_kernel_to_qwen3  # noqa: F401
 
 
 # Check if 'transformers' is installed
@@ -93,6 +94,7 @@ def __getattr__(name: str):
         "apply_liger_kernel_to_qwen2",
         "apply_liger_kernel_to_qwen2_5_vl",
         "apply_liger_kernel_to_qwen2_vl",
+        "apply_liger_kernel_to_qwen3",
     }
 
     if name in monkey_patch_symbols:
@@ -144,5 +146,6 @@ if _TRANSFORMERS_AVAILABLE:
             "apply_liger_kernel_to_qwen2",
             "apply_liger_kernel_to_qwen2_5_vl",
             "apply_liger_kernel_to_qwen2_vl",
+            "apply_liger_kernel_to_qwen3",
         ]
     )
