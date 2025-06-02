8926fe61af9ac39b4da2abaeda1c47c99c9e9e19
KareemMusleh <prototribe@protonmail.com>
2025-06-02 17:11:07 +0700

    oops

--- test/utils.py
+++ test/utils.py
@@ -457,10 +457,10 @@ def revert_liger_kernel_to_solar(model_config: MiniModelConfig):
     Revert all Liger kernel patches applied to Solar.
     """
 
-    from liger_kernel.transformers.model import modeling_glm4
+    from liger_kernel.transformers.model import modeling_solar
 
-    importlib.reload(modeling_glm4)
-    model_config.model_class = modeling_glm4.Glm4ForCausalLM
+    importlib.reload(modeling_solar)
+    model_config.model_class = modeling_solar.SolarForCausalLM
     print("Liger kernel patches have been reverted.")
 
 
