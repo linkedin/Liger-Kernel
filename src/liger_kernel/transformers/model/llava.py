f6751bbfe869bf34ceac67a54f97a6da62455f6a
Manan17 <shahmanan170602@gmail.com>
2025-06-08 23:59:08 +0000

    Fixup

--- src/liger_kernel/transformers/model/llava.py
+++ src/liger_kernel/transformers/model/llava.py
@@ -310,20 +310,11 @@ def lce_forward(
         )
 
     else:
-        logits = self.lm_head(hidden_states)
+        logits = self.lm_head(kept_hidden_states)
         if labels is not None:
-            # Upcast to float if we need to compute the loss to avoid potential precision issues
-            logits = logits.float()
-            # Shift so that tokens < n predict n
-            shift_logits = logits[..., :-1, :].contiguous()
-            shift_labels = labels[..., 1:].contiguous()
-            # Flatten the tokens
-            loss_fct = CrossEntropyLoss()
-            shift_logits = shift_logits.view(-1, self.config.vocab_size)
-            shift_labels = shift_labels.view(-1)
-            # Enable model parallelism
-            shift_labels = shift_labels.to(shift_logits.device)
-            loss = loss_fct(shift_logits, shift_labels)
+            loss = self.loss_function(
+                logits=logits, labels=labels, vocab_size=self.config.text_config.vocab_size, **lm_kwargs
+            )
 
 
     if not return_dict:
