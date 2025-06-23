649a5dc02abc832b47d2f4184e6c8d676dd0fd6a
Manan17 <shahmanan170602@gmail.com>
2025-06-23 18:09:25 +0000

    strict requirement to H100

--- dev/modal/benchmarks.py
+++ dev/modal/benchmarks.py
@@ -14,7 +14,7 @@ app = modal.App("liger_benchmarks", image=image)
 repo = image.add_local_dir(ROOT_PATH, remote_path=REMOTE_ROOT_PATH)
 
 
-@app.function(gpu=modal.gpu.H100, image=repo, timeout=60 * 90)
+@app.function(gpu="100", image=repo, timeout=60 * 90)
 def liger_benchmarks():
     import os
     import subprocess
