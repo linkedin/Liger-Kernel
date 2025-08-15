1d23e968ddc9b937fa1973ffc4f39f10004a3ccb
EddieTsai <vvvdwbvvv@gmail.com>
2025-08-15 19:11:16 +0800

    feat(utils): add revert function for Glm4v kernel patches

--- test/utils.py
+++ test/utils.py
@@ -517,6 +517,7 @@ def revert_liger_kernel_to_glm4(model_config: MiniModelConfig):
     model_config.model_class = modeling_glm4.Glm4ForCausalLM
     print("Liger kernel patches have been reverted.")
 
+
 def revert_liger_kernel_to_glm4v(model_config: MiniModelConfig):
     """
     Revert all Liger kernel patches applied to Glm4v.
@@ -527,6 +528,7 @@ def revert_liger_kernel_to_glm4v(model_config: MiniModelConfig):
     importlib.reload(modeling_glm4v)
     model_config.model_class = modeling_glm4v.Glm4vForConditionalGeneration
     print("Liger kernel patches have been reverted.")
+
 
 def revert_liger_kernel_to_llava(model_config: MiniModelConfig):
     """
