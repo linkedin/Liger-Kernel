0957e0279a614487d4747399122a9a2c548c067c
Parag Ekbote <thecoolekbote189@gmail.com>
2025-09-19 17:25:17 +0000

    update.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_optional_dependencies():
         "dev": [
             "transformers>=4.49.0",
             "matplotlib>=3.7.2",
-            "ruff.=0.12.0",
+            "ruff>=0.12.0",
             "pytest>=7.1.2",
             "pytest-xdist",
             "pytest-cov",
