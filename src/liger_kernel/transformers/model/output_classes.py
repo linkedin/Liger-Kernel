002c0eca99464abe037d56fd6c0b8e2254ea103d
Kashif Rasul <kashif.rasul@gmail.com>
2025-10-17 09:51:59 +0000

    add missing output class

---
+++ src/liger_kernel/transformers/model/output_classes.py
@@ -0,0 +1,64 @@
+"""
+Custom output classes for Liger-Kernel that extend transformers' ModelOutput classes
+with optional accuracy field.
+"""
+
+from dataclasses import dataclass
+from typing import Optional
+
+import torch
+
+from transformers.modeling_outputs import CausalLMOutputWithPast
+from transformers.modeling_outputs import MoeCausalLMOutputWithPast
+from transformers.models.gemma3.modeling_gemma3 import Gemma3CausalLMOutputWithPast
+from transformers.models.glm4v_moe.modeling_glm4v_moe import Glm4vMoeCausalLMOutputWithPast
+from transformers.models.internvl.modeling_internvl import InternVLCausalLMOutputWithPast
+from transformers.models.llava.modeling_llava import LlavaCausalLMOutputWithPast
+from transformers.models.paligemma.modeling_paligemma import PaliGemmaCausalLMOutputWithPast
+from transformers.models.qwen2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLCausalLMOutputWithPast
+from transformers.models.qwen2_vl.modeling_qwen2_vl import Qwen2VLCausalLMOutputWithPast
+
+
+@dataclass
+class LigerCausalLMOutputWithPast(CausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerMoeCausalLMOutputWithPast(MoeCausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerGemma3CausalLMOutputWithPast(Gemma3CausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerGlm4vMoeCausalLMOutputWithPast(Glm4vMoeCausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerLlavaCausalLMOutputWithPast(LlavaCausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerInternVLCausalLMOutputWithPast(InternVLCausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerPaliGemmaCausalLMOutputWithPast(PaliGemmaCausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerQwen2_5_VLCausalLMOutputWithPast(Qwen2_5_VLCausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class LigerQwen2VLCausalLMOutputWithPast(Qwen2VLCausalLMOutputWithPast):
+    token_accuracy: Optional[torch.FloatTensor] = None
