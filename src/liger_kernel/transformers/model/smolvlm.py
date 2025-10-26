4cb7389d553d641361ba821d8d0b1fc86b49f709
MilkClouds <milkclouds00@gmail.com>
2025-10-26 19:04:05 +0900

    Update src/liger_kernel/transformers/model/smolvlm.py
    Co-authored-by: Tcc0403 <76503978+Tcc0403@users.noreply.github.com>

--- src/liger_kernel/transformers/model/smolvlm.py
+++ src/liger_kernel/transformers/model/smolvlm.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 from typing import Optional
 from typing import Union
-from typing import Unpack
+from transformers.processing_utils import Unpack
 
 import torch
 
