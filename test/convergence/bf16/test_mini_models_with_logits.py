8fad470fcde63f59bd7145c4b866b077de166ce9
Tcc0403 <76503978+Tcc0403@users.noreply.github.com>
2025-11-01 01:00:35 +0800

    fix(ci): modify Glm4vMoe config for convergence test (#918)
    ## Summary
    <!--- This is a required section; please describe the main purpose of
    this proposed code change. --->
    Convergence test for Glm4vMoe takes too long and is difficult to pass.
    This PR changes two config to address above issue:
    1. Lower `n_routed_experts` to reduce model size
    ([41.94s](https://github.com/linkedin/Liger-Kernel/actions/runs/18782716832/job/53592891039#step:5:5493)
    ->
    [9.45s](https://github.com/linkedin/Liger-Kernel/actions/runs/18796804589/job/53696472240#step:5:5696))
    ```
    ============================== slowest durations ===============================
    56.92s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_glm4v_moe-32-1e-05-dtype17-0.01-0.01-0.1-0.01-0.01-0.01]
    19.38s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_qwen3_next-32-1e-05-dtype22-0.01-0.01-0.1-0.1-0.01-0.01]
    14.26s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_llama4-32-1e-05-dtype0-0.01-0.05-0.3-0.2-0.01-0.01]
    10.72s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_qwen3_moe-32-1e-05-dtype7-0.01-0.05-0.1-0.01-0.01-0.01]
    9.04s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_qwen2_vl-32-1e-05-dtype8-0.01-0.05-0.1-0.01-0.01-0.01]
    6.85s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_qwen2_5_vl-32-1e-05-dtype9-0.01-0.01-0.1-0.01-0.01-0.01]
    5.91s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_granite3-32-1e-05-dtype3-0.01-0.01-0.1-0.01-0.01-0.01]
    5.69s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_gemma3_text-32-1e-05-dtype18-0.01-0.05-0.3-0.01-0.01-0.01]
    5.59s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_falcon_h1-32-1e-05-dtype21-0.01-0.01-0.1-0.01-0.01-0.01]
    5.47s call     test/convergence/bf16/test_mini_models_with_logits.py::test_mini_model[mini_llava-32-1e-05-dtype2-0.01-0.05-0.1-0.01-0.01-0.01]
    ```
    3. Match the tolerance numbers in both files
    <!---
    ## Details
    This is an optional section; is there anything specific that reviewers
    should be aware of?
    --->
    
    ## Testing Done
    <!--- This is a required section; please describe how this change was
    tested. --->
    
    <!-- 
    Replace BLANK with your device type. For example, A100-80G-PCIe
    
    Complete the following tasks before sending your PR, and replace `[ ]`
    with
    `[x]` to indicate you have done them. 
    -->
    
    - Hardware Type: <BLANK>
    - [ ] run `make test` to ensure correctness
    - [ ] run `make checkstyle` to ensure code style
    - [ ] run `make test-convergence` to ensure convergence
    
    ---------
    
    Signed-off-by: Tcc0403 <76503978+Tcc0403@users.noreply.github.com>

--- test/convergence/bf16/test_mini_models_with_logits.py
+++ test/convergence/bf16/test_mini_models_with_logits.py
@@ -1003,7 +1003,7 @@ if GLM4V_MOE_AVAILABLE:
                 "moe_intermediate_size": 1408,
                 "num_experts_per_tok": 2,
                 "n_shared_experts": 1,
-                "n_routed_experts": 128,
+                "n_routed_experts": 8,
                 "routed_scaling_factor": 1.0,
                 "n_group": 1,
                 "topk_group": 1,
@@ -1557,9 +1557,9 @@ def run_mini_model(
             1e-5,
             torch.bfloat16,
             1e-2,
-            1e-2,
+            4e-1,  # rms_norm patch needs higher tolerance in bf16
             1e-1,
-            1e-2,
+            5e-1,  # rms_norm patch needs higher tolerance in bf16
             1e-2,
             1e-2,
             marks=[
