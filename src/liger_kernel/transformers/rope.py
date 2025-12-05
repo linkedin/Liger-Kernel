7558249e4096f1e0bc3c5ddea883c4abbbbf5532
Tcc0403 <76503978+Tcc0403@users.noreply.github.com>
2025-12-05 20:31:30 +0800

    Fix qwen3vl `apply_rotary_pos_emb_vision`
    Signed-off-by: Tcc0403 <76503978+Tcc0403@users.noreply.github.com>

--- src/liger_kernel/transformers/rope.py
+++ src/liger_kernel/transformers/rope.py
@@ -1,4 +1,3 @@
-from typing import Optional
 from typing import Tuple
 
 import torch
@@ -25,39 +24,37 @@ def liger_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
     return LigerRopeFunction.apply(q, k, cos, sin, position_ids, unsqueeze_dim)
 
 
-def liger_rotary_pos_emb_with_cast(
+def liger_rotary_pos_emb_vision(
     q: torch.Tensor,
     k: torch.Tensor,
     cos: torch.Tensor,
     sin: torch.Tensor,
-    position_ids: Optional[torch.Tensor] = None,
-    unsqueeze_dim: int = 1,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Modified version of liger_rotary_pos_emb for qwen3_vl's apply_rotary_pos_emb_vision function.
+    Manually tranposed the input and output to match the expected shape for liger_rotary_pos_emb.
+    Reference: https://https://github.com/huggingface/transformers/blob/v5.0.0rc0/src/transformers/models/qwen3_vl/modeling_qwen3_vl.py#L116
+
+    Args:
+        q (torch.Tensor): The query tensor of shape (seq_length, num_heads, head_dim),
+        with stride (num_heads * head_dim, head_dim, 1).
+        k (torch.Tensor): The query tensor of shape (seq_length, num_heads, head_dim),
+        with stride (num_heads * head_dim, head_dim, 1). Same as q.
+        cos (torch.Tensor): The cosine tensor of shape (seq_length, head_dim).
+        sin (torch.Tensor): The sine tensor of shape (seq_length, head_dim).
+    """
+    print(f"{q.shape=}, {k.shape=}")
+    print(f"{cos.shape=}, {sin.shape=}")
+    print(f"{q.stride()=}, {k.stride()=}")
+    print(f"{cos.stride()=}, {sin.stride()=}")
     orig_q_dtype, orig_k_dtype = q.dtype, k.dtype
 
-    q32 = q.to(torch.float32)
-    k32 = k.to(torch.float32)
+    q32 = q.to(torch.float32).unsqueeze(0).transpose(1, 2)  # (1, num_heads, seq_length, head_dim)
+    k32 = k.to(torch.float32).unsqueeze(0).transpose(1, 2)  # (1, num_heads, seq_length, head_dim)
     cos32 = cos.to(torch.float32)
     sin32 = sin.to(torch.float32)
 
-    q_out, k_out = liger_rotary_pos_emb(q32, k32, cos32, sin32, position_ids=position_ids, unsqueeze_dim=unsqueeze_dim)
-    return q_out.to(orig_q_dtype), k_out.to(orig_k_dtype)
-
-
-def liger_rotary_pos_emb_with_cast_and_leading_batch(
-    q: torch.Tensor,
-    k: torch.Tensor,
-    cos: torch.Tensor,
-    sin: torch.Tensor,
-    position_ids: Optional[torch.Tensor] = None,
-    unsqueeze_dim: int = 1,
-) -> Tuple[torch.Tensor, torch.Tensor]:
-    orig_q_dtype, orig_k_dtype = q.dtype, k.dtype
-
-    q32 = q.to(torch.float32).unsqueeze(0)
-    k32 = k.to(torch.float32).unsqueeze(0)
-    cos32 = cos.to(torch.float32).unsqueeze(0)
-    sin32 = sin.to(torch.float32).unsqueeze(0)
-
-    q_out, k_out = liger_rotary_pos_emb(q32, k32, cos32, sin32, position_ids=position_ids, unsqueeze_dim=unsqueeze_dim)
-    return q_out.to(orig_q_dtype).squeeze(0), k_out.to(orig_k_dtype).squeeze(0)
+    q_out, k_out = liger_rotary_pos_emb(q32, k32, cos32, sin32)
+    q_out = q_out.transpose(1, 2).squeeze(0).to(orig_q_dtype)
+    k_out = k_out.transpose(1, 2).squeeze(0).to(orig_q_dtype)
+    return q_out, k_out
