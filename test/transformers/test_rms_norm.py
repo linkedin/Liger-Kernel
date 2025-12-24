a71024302bfe7155cb2837c2f2c0d1efe99a2c2d
niyunsheng <ni-ys13@tsinghua.org.cn>
2025-12-24 17:09:38 +0800

    add elementwise_affine param for rms norm

--- test/transformers/test_rms_norm.py
+++ test/transformers/test_rms_norm.py
@@ -31,26 +31,37 @@ SLEEP_SECONDS = 0.1
 
 
 class BaseRMSNorm(nn.Module):
-    def __init__(self, hidden_size, eps=1e-6):
+    def __init__(self, hidden_size, eps=1e-6, elementwise_affine=True):
         super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.elementwise_affine = elementwise_affine
+        if self.elementwise_affine:
+            self.weight = nn.Parameter(torch.ones(hidden_size))
+        else:
+            self.register_parameter("weight", None)
         self.variance_epsilon = eps
 
     def forward(self, hidden_states):
         input_dtype = hidden_states.dtype
         variance = hidden_states.pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
+        if self.elementwise_affine:
+            return self.weight * hidden_states.to(input_dtype)
+        else:
+            return hidden_states.to(input_dtype)
 
 
 # https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py#L112
 class LlamaRMSNorm(nn.Module):
-    def __init__(self, hidden_size, eps=1e-6):
+    def __init__(self, hidden_size, eps=1e-6, elementwise_affine=True):
         """
         LlamaRMSNorm is equivalent to T5LayerNorm
         """
         super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.elementwise_affine = elementwise_affine
+        if self.elementwise_affine:
+            self.weight = nn.Parameter(torch.ones(hidden_size))
+        else:
+            self.register_parameter("weight", None)
         self.variance_epsilon = eps
 
     def forward(self, hidden_states):
@@ -58,7 +69,10 @@ class LlamaRMSNorm(nn.Module):
         hidden_states = hidden_states.to(torch.float32)
         variance = hidden_states.pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
+        if self.elementwise_affine:
+            return self.weight * hidden_states.to(input_dtype)
+        else:
+            return hidden_states.to(input_dtype)
 
 
 # https://github.com/huggingface/transformers/blob/v4.44.2/src/transformers/models/gemma/modeling_gemma.py#L122
@@ -118,7 +132,14 @@ class GemmaRMSNorm(nn.Module):
         False,
     ],
 )
-def test_correctness(bs, sl, hd, dtype, atol, rtol, reference, offset, casting_mode, in_place):
+@pytest.mark.parametrize(
+    "elementwise_affine",
+    [
+        True,
+        False,
+    ],
+)
+def test_correctness(bs, sl, hd, dtype, atol, rtol, reference, offset, casting_mode, in_place, elementwise_affine):
     _tensor = torch.randn(bs, sl, hd, device=device, dtype=dtype)
 
     h1 = _tensor.clone().requires_grad_(True)
@@ -128,19 +149,33 @@ def test_correctness(bs, sl, hd, dtype, atol, rtol, reference, offset, casting_m
     do = torch.randn(bs, sl, hd, device=device, dtype=dtype)
 
     # reference (llama or gemma)
-    ref_rms = reference(hidden_size=hd).to(device).to(dtype)
+    if not elementwise_affine and reference == GemmaRMSNorm:
+        return pytest.skip("GemmaRMSNorm does not support elementwise_affine=False")
+    if reference == GemmaRMSNorm:
+        ref_rms = reference(hidden_size=hd).to(device).to(dtype)
+    else:
+        ref_rms = reference(hidden_size=hd, elementwise_affine=elementwise_affine).to(device).to(dtype)
     ref_o = ref_rms(h1)
     ref_o.backward(do, retain_graph=True)
 
     # triton
     triton_rms = (
-        LigerRMSNorm(hidden_size=hd, offset=offset, casting_mode=casting_mode, in_place=in_place).to(device).to(dtype)
+        LigerRMSNorm(
+            hidden_size=hd,
+            offset=offset,
+            casting_mode=casting_mode,
+            in_place=in_place,
+            elementwise_affine=elementwise_affine,
+        )
+        .to(device)
+        .to(dtype)
     )
     triton_o = triton_rms(h2)
     triton_o.backward(do, retain_graph=True)
 
     assert_verbose_allclose(ref_o, triton_o, atol=atol, rtol=rtol)
-    assert_verbose_allclose(ref_rms.weight.grad, triton_rms.weight.grad, atol=atol, rtol=rtol)
+    if elementwise_affine:
+        assert_verbose_allclose(ref_rms.weight.grad, triton_rms.weight.grad, atol=atol, rtol=rtol)
     print(f"{h1.grad=}")
     print(f"{h2.grad=}")
     assert_verbose_allclose(h1.grad, h2.grad, atol=atol, rtol=rtol, max_print=20)
@@ -168,14 +203,24 @@ def test_correctness(bs, sl, hd, dtype, atol, rtol, reference, offset, casting_m
         (GemmaRMSNorm, 1.0, "gemma"),
     ],
 )
-def test_correctness_functional(bs, sl, hd, dtype, atol, rtol, reference, offset, casting_mode):
+@pytest.mark.parametrize(
+    "elementwise_affine",
+    [
+        True,
+        False,
+    ],
+)
+def test_correctness_functional(bs, sl, hd, dtype, atol, rtol, reference, offset, casting_mode, elementwise_affine):
     # h
     _tensor = torch.randn(bs, sl, hd, device=device, dtype=dtype)
 
     h1 = _tensor.clone().requires_grad_(True)
     h2 = _tensor.clone().requires_grad_(True)
 
-    w = torch.randn(hd, device=device, dtype=dtype)
+    if elementwise_affine:
+        w = torch.randn(hd, device=device, dtype=dtype)
+    else:
+        w = None
 
     y1 = liger_rms_norm(X=h1, W=w, eps=1e-6, offset=offset, casting_mode=casting_mode)
     y2 = LigerRMSNormFunction.apply(h2, w, 1e-6, offset, casting_mode)
