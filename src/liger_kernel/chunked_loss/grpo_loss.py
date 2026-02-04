Fix incorrect dim expansion during `is_clipped` computation
Signed-off-by: Tcc0403 <76503978+Tcc0403@users.noreply.github.com>

src/liger_kernel/chunked_loss/grpo_loss.py
@@ -130,7 +130,9 @@ class LigerFusedLinearGRPOFunction(LigerFusedLinearPPOBase):
             )
         else:  # sequence level
             # For sequence level, coef_1 is shape (B, 1), advantages is shape (B,)
-            is_clipped = (is_lower_clipped & (advantages < 0)) | (is_upper_clipped & (advantages > 0))
+            is_clipped = (is_lower_clipped & (advantages.unsqueeze(1) < 0)) | (
+                is_upper_clipped & (advantages.unsqueeze(1) > 0)
+            )
             is_clipped = is_clipped.expand_as(attention_mask)
 
         metrics.append((is_clipped * attention_mask).sum() / torch.clamp(full_attention_mask.sum(), min=1.0))

test/chunked_loss/test_grpo_loss.py
@@ -113,7 +113,7 @@ class TorchLMHeadGRPO(torch.nn.Module):
             is_clipped = (is_lower_clipped & (expanded_advantages < 0)) | (is_upper_clipped & (expanded_advantages > 0))
         else:  # sequence level
             # For sequence level, coef_1 is shape (B, 1), advantages is shape (B,)
-            is_clipped = (is_lower_clipped & (advantages < 0)) | (is_upper_clipped & (advantages > 0))
+            is_clipped = (is_lower_clipped & (expanded_advantages < 0)) | (is_upper_clipped & (expanded_advantages > 0))
             is_clipped = is_clipped.expand_as(attention_mask)
         return per_token_loss, kl_div, is_clipped
 