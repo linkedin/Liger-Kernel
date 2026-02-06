f6ec79ef0a6be8b9b6e6bff983d5810fc475ad7e
Michael Lai <michaellai901026@gmail.com>
2026-02-06 19:42:32 +0000

    add unit test for swiglu mixtral & fix code

--- src/liger_kernel/transformers/swiglu.py
+++ src/liger_kernel/transformers/swiglu.py
@@ -68,7 +68,7 @@ class LigerExperts(nn.Module):
             top_k_pos, token_idx = torch.where(expert_mask[expert_idx])
             current_state = hidden_states[token_idx]
             gate, up = nn.functional.linear(current_state, self.gate_up_proj[expert_idx]).chunk(2, dim=-1)
-            current_hidden_states = LigerSiLUMulFunction(gate) * up
+            current_hidden_states = LigerSiLUMulFunction.apply(gate, up)
             current_hidden_states = nn.functional.linear(current_hidden_states, self.down_proj[expert_idx])
             current_hidden_states = current_hidden_states * top_k_weights[token_idx, top_k_pos, None]
             final_hidden_states.index_add_(0, token_idx, current_hidden_states.to(final_hidden_states.dtype))
