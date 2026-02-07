183c4db892ca31b3a02305288ac0abdabe64c9cb
Tcc0403 <76503978+Tcc0403@users.noreply.github.com>
2026-02-07 18:48:13 +0800

    Decouple tests ci for different transformers versions (#1083)
    ## Summary
    <!--- This is a required section; please describe the main purpose of
    this proposed code change. --->
    
    <!---
    ## Details
    This is an optional section; is there anything specific that reviewers
    should be aware of?
    --->
    
    ## Testing Done
    <!--- This is a required section; please describe how this change was
    tested. --->
    
    <!-- 
    Replace BLANK with your device type. For example, A100-80G-PCIe
    
    Complete the following tasks before sending your PR, and replace `[ ]`
    with
    `[x]` to indicate you have done them. 
    -->
    
    - Hardware Type: <BLANK>
    - [ ] run `make test` to ensure correctness
    - [ ] run `make checkstyle` to ensure code style
    - [ ] run `make test-convergence` to ensure convergence
    
    Signed-off-by: Tcc0403 <76503978+Tcc0403@users.noreply.github.com>

--- dev/modal/tests.py
+++ dev/modal/tests.py
@@ -6,6 +6,8 @@ ROOT_PATH = Path(__file__).parent.parent.parent
 REMOTE_ROOT_PATH = "/root/liger-kernel"
 PYTHON_VERSION = "3.12"
 
+OLDEST_SUPPORTED_TRANSFORMERS_V4_VERSION = "4.49.0"
+
 image = modal.Image.debian_slim(python_version=PYTHON_VERSION).pip_install("uv")
 
 app = modal.App("liger_tests", image=image)
@@ -15,7 +17,7 @@ repo = image.add_local_dir(ROOT_PATH, remote_path=REMOTE_ROOT_PATH)
 
 
 @app.function(gpu="H100!", image=repo, timeout=90 * 60)
-def liger_tests():
+def liger_correctness_tests():
     import subprocess
 
     subprocess.run(
@@ -24,5 +26,99 @@ def liger_tests():
         shell=True,
         cwd=REMOTE_ROOT_PATH,
     )
+    subprocess.run(["make test-convergence"], check=True, shell=True, cwd=REMOTE_ROOT_PATH)
+
+
+@app.function(gpu="H100!", image=repo, timeout=90 * 60)
+def liger_convergence_tests():
+    import subprocess
+
+    subprocess.run(
+        ["uv pip install -e '.[dev]' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
+    subprocess.run(["make test-convergence"], check=True, shell=True, cwd=REMOTE_ROOT_PATH)
+
+
+oldest_v4_app = modal.App("liger_oldest_v4_tests", image=image)  # 4.49.0
+
+
+@oldest_v4_app.function(gpu="H100!", image=repo, timeout=90 * 60)
+def liger_oldest_v4_correctness_tests():
+    import subprocess
+
+    subprocess.run(
+        ["uv pip install -e '.[dev]' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
+    subprocess.run(
+        [f"uv pip install 'transformers=={OLDEST_SUPPORTED_TRANSFORMERS_V4_VERSION}' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
     subprocess.run(["make test"], check=True, shell=True, cwd=REMOTE_ROOT_PATH)
+
+
+@oldest_v4_app.function(gpu="H100!", image=repo, timeout=90 * 60)
+def liger_oldest_v4_convergence_tests():
+    import subprocess
+
+    subprocess.run(
+        ["uv pip install -e '.[dev]' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
+    subprocess.run(
+        [f"uv pip install 'transformers=={OLDEST_SUPPORTED_TRANSFORMERS_V4_VERSION}' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
+    subprocess.run(["make test-convergence"], check=True, shell=True, cwd=REMOTE_ROOT_PATH)
+
+
+latest_v4_app = modal.App("liger_latest_v4_tests", image=image)
+
+
+@latest_v4_app.function(gpu="H100!", image=repo, timeout=90 * 60)
+def liger_latest_v4_correctness_tests():
+    import subprocess
+
+    subprocess.run(
+        ["uv pip install -e '.[dev]' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
+    subprocess.run(
+        [f"uv pip install 'transformers>={OLDEST_SUPPORTED_TRANSFORMERS_V4_VERSION}, <5.0.0' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
+    subprocess.run(["make test"], check=True, shell=True, cwd=REMOTE_ROOT_PATH)
+
+
+@latest_v4_app.function(gpu="H100!", image=repo, timeout=90 * 60)
+def liger_latest_v4_convergence_tests():
+    import subprocess
+
+    subprocess.run(
+        ["uv pip install -e '.[dev]' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
+    subprocess.run(
+        [f"uv pip install 'transformers>={OLDEST_SUPPORTED_TRANSFORMERS_V4_VERSION}, <5.0.0' --system"],
+        check=True,
+        shell=True,
+        cwd=REMOTE_ROOT_PATH,
+    )
     subprocess.run(["make test-convergence"], check=True, shell=True, cwd=REMOTE_ROOT_PATH)
