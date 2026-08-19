Fix potential silent error in mlp_bias
Signed-off-by: ma-jh <3364870135@qq.com>

src/liger_kernel/transformers/mlp.py
@@ -34,10 +34,11 @@ class LigerFalconH1MLP(nn.Module):
         self.config = config
         self.hidden_size = config.hidden_size
         self.intermediate_size = config.intermediate_size
-        bias = getattr(config, "mlp_bias", False)
-        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=bias)
-        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=bias)
-        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=bias)
+        if getattr(config, "mlp_bias", False):
+            raise ValueError("LigerFalconH1MLP does not support bias")
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
         if config.hidden_act not in ["silu", "swish"]:
             raise ValueError(f"Activation function {config.hidden_act} not supported.")
         gate_multiplier, down_multiplier = config.mlp_multipliers